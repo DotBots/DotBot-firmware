3df20b8378cc3717b79af95827b29d115f8d5d2c
Said Alvarado <said_alvarado@hotmail.com>
2022-01-06 17:14:36 +0100

    #10 Cleaned comment and documentation in the code.

--- projects/00std_led/00std_led.c
+++ projects/00std_led/00std_led.c
@@ -1,74 +1,73 @@
 /*********************************************************************
-*                    SEGGER Microcontroller GmbH                     *
-*                        The Embedded Experts                        *
-**********************************************************************
+*                                                                    * 
+*                               DotBot                               *
+*                                                                    *
+**********************************************************************/
 
--------------------------- END-OF-HEADER -----------------------------
-
-File    : main.c
-Purpose : Generic application start
-
-*/
-
+/**
+ * @file 00std_led.c
+ * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
+ * @brief This is a short example of how to interface with the RGB LED in the DotBot board.
+ * 
+ * Load this program on your board. The LEDs should start blinking different colors.
+ * 
+ * @version 0.1
+ * @date 2022-01-06
+ * 
+ * @copyright INRIA, 2022
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <nrf.h>
-// #include "nrf52840_bitfields.h"
-// #include "nrf52840.h"
 
-// Pin definition
-#define MOSI_PIN 3   // port 0
-//#define MISO_PIN 4
-#define SCK_PIN  6   // port 1  ( used because it's not an available pin in the BCM module)
-//#define CS_PIN   29
 
-// EasyDMA buffer definition
+//=========================== defines =========================================
+// Pin definitions
+#define MOSI_PIN 3   ///< nRF52840 P0.3
+#define SCK_PIN 6    ///< nRF52840 P1.6 ( used because it's not an available pin in the BCM module).
+
+// EasyDMA buffer size definition
 #define READERBUFFER_SIZE 32
 #define WRITERBUFFER_SIZE 32
-
-uint8_t readerBuffer[READERBUFFER_SIZE];
-uint8_t writerBuffer[WRITERBUFFER_SIZE] = {0x00, 0x84, 0x29, 0x4A, 0x42, 0x90, 0xA4, 0x29, 0x08, 0x42, 0x10, 0x84, 0x21, 0x08, 0x42, 0x10, 0xA5, 0x29, 0x4A, 0x52, 0x94, 0xA5, 0x29, 0x48, 0x42, 0x10, 0x84, 0x21, 0x08, 0x42, 0x10, 0x00};
-
-// EasyDMA buffer for the RGB LED.
-uint8_t ledBuffer[WRITERBUFFER_SIZE];
 
 // Define the ONE and ZERO enconding for the led one-wire protocol
 #define LED_ONE  0b00010100
 #define LED_ZERO 0b00010000
 
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
+
+//=========================== variables =========================================
+// EasyDMA buffer declaration (preloaded with a RED led command, just as an example, it's not actually used)
+uint8_t readerBuffer[READERBUFFER_SIZE];
+uint8_t writerBuffer[WRITERBUFFER_SIZE] = {0x00, 0x84, 0x29, 0x4A, 0x42, 0x90, 0xA4, 0x29, 0x08, 0x42, 0x10, 0x84, 0x21, 0x08, 0x42, 0x10, 0xA5, 0x29, 0x4A, 0x52, 0x94, 0xA5, 0x29, 0x48, 0x42, 0x10, 0x84, 0x21, 0x08, 0x42, 0x10, 0x00};
+
+// EasyDMA buffer declaration for the RGB LED.
+uint8_t ledBuffer[WRITERBUFFER_SIZE];
 
 
+//=========================== prototypes =========================================
 void set_rgb_led(uint8_t r, uint8_t g, uint8_t b);
 
 
+//=========================== public =========================================
 
+/**
+*  @brief The program starts executing here.
+*/
 int main(void)
 {
 
-  // ********** Test the Board ************
+  // Turn ON the DotBot board regulator 
   NRF_P0->DIRSET = 1 << 20;  
-  NRF_P0->OUTSET = 1 << 20;  
-  //NRF_P0->OUTCLR = 1 << 14;  
+  NRF_P0->OUTSET = 1 << 20;   
 
-  NRF_P0->DIRSET = 1 << 31;  
-
-// ********** Configure the necessary Pins in the GPIO peripheral ************
-
+  // Configure the necessary Pins in the GPIO peripheral 
   NRF_P0->DIRSET = 1 << MOSI_PIN;  // MOSI as Output
   //NRF_P0->DIRCLR = 1 << MISO_PIN;  // MISO as Input
   NRF_P0->DIRSET = 1 << SCK_PIN;   // SCK  as Output
   //NRF_P0->DIRSET = 1 << CS_PIN;    // CS   as Output
 
-
-// ********** Define the necessary Pins in the SPIM peripheral ************
-
+  // Define the necessary Pins in the SPIM peripheral 
   NRF_SPIM0->PSEL.MOSI =  MOSI_PIN << SPIM_PSEL_MOSI_PIN_Pos  |                           // Define pin number for MOSI pin
                           0        << SPIM_PSEL_MOSI_PORT_Pos |                           // Define pin port for MOSI pin
                           SPIM_PSEL_MOSI_CONNECT_Connected << SPIM_PSEL_MOSI_CONNECT_Pos; // Enable the MOSI pin
@@ -77,46 +76,45 @@ int main(void)
                          1        << SPIM_PSEL_SCK_PORT_Pos |                             // Define pin port for SCK pin
                          SPIM_PSEL_SCK_CONNECT_Connected << SPIM_PSEL_SCK_CONNECT_Pos;    // Enable the SCK pin
 
-// ********** Configure the SPIM peripheral ************
-
+  // Configure the SPIM peripheral 
   NRF_SPIM0->FREQUENCY = SPIM_FREQUENCY_FREQUENCY_K500;                        // Set SPI frequency to 500Khz
   NRF_SPIM0->CONFIG    = SPIM_CONFIG_ORDER_MsbFirst << SPIM_CONFIG_ORDER_Pos;  // Set MsB out first
 
-
-// ********** Configure the EasyDMA channel ************
-
+  // Configure the EasyDMA channel 
   // Configuring the READER channel
   NRF_SPIM0->RXD.MAXCNT = READERBUFFER_SIZE;              // Set the size of the input buffer.
   NRF_SPIM0->RXD.PTR    = &readerBuffer;                  // Set the input buffer pointer.
-
   // Configure the WRITER channel
-  NRF_SPIM0->TXD.MAXCNT = WRITERBUFFER_SIZE;              // Set the size of the input buffer.
-  NRF_SPIM0->TXD.PTR    = &writerBuffer;                  // Set the input buffer pointer.
+  NRF_SPIM0->TXD.MAXCNT = WRITERBUFFER_SIZE;              // Set the size of the output buffer.
+  NRF_SPIM0->TXD.PTR    = &writerBuffer;                  // Set the output buffer pointer.
 
+  // Enable the SPIM pripheral 
+  NRF_SPIM0->ENABLE = SPIM_ENABLE_ENABLE_Enabled << SPIM_ENABLE_ENABLE_Pos;
 
-  // ********** Enable the SPIM pripheral ************
-
-  NRF_SPIM0->ENABLE = SPIM_ENABLE_ENABLE_Enabled << SPIM_ENABLE_ENABLE_Pos;  // Enable the SPI periperal.
-
-
-  // ********** Set color of the LED ****************
-  NRF_P0->OUTSET = 1 << 31;
+  // Set color of the LED
   set_rgb_led(0,50,50);
-  NRF_P0->OUTCLR = 1 << 31;
 
-  // ********** Execute a transfer ************
-
+  // Execute a transfer 
   NRF_SPIM0->TASKS_START = SPIM_TASKS_START_TASKS_START_Trigger << SPIM_TASKS_START_TASKS_START_Pos;  // trigger the SPI transfer
 
-  // one last instruction.
+  // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
   NRF_P0->DIRSET = 1 << 14;
 }
 
-
+/**
+ * @brief Set the color to display in the RGB LED
+ * 
+ *  Assembles the 1-wire encoded command to change the color of the RGB led.
+ *  Afterward, an SPI transfer is necessary to actually update the LED color.
+ * 
+ * @param[in] r red value of the led color [0 - 255]
+ * @param[in] g green value of the led color [0 - 255]
+ * @param[in] b blue value of the led color [0 - 255]
+ */
 void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
 
  
-  // Load the write command on to the buffer.
+  // Load the obligatory starting write command on to the buffer.
   ledBuffer[0] = 0x00;
   ledBuffer[1] = 0x84;
   ledBuffer[2] = 0x29;
@@ -128,7 +126,7 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
   ledBuffer[8] = 0x00;
 
 
-  // ********** Load the blue into the buffer ************
+  // ********** Load the Blue value into the buffer ************
 
   // Define the starting position of the blue color in the buffer.
   int buffer_byte = 8;
@@ -144,7 +142,7 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
       // iterate over all the bits in the 5bit encoded '1'.
       for (int j = 4; j >= 0; j--) 
       {
-        // Extract a single (j) bit from the '1' code, right shift it to the corect position, and write into the buffer.
+        // Extract a single (j) bit from the '1' code, left shift it to the corect position, and write into the buffer.
         ledBuffer[buffer_byte] |= ((LED_ONE >> j) & 0x01) << (buffer_bit % 8);
         // move the buffer bit position we are writting to, a single bit to the right.
         buffer_bit--;
@@ -163,15 +161,14 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
       }
     }
   }
-
-  // fill the remaining 4 bytes with encoded zeros
+  // fill the remaining 4 bytes of the 1-wire command with encoded zeros
   ledBuffer[13] |= 0x08;
   ledBuffer[14] = 0x42;
   ledBuffer[15] = 0x10;
 
 
 
-  // ********** Load the red into the buffer ************
+  // ********** Load the Red value into the buffer ************
 
   // Define the starting position of the blue color in the buffer.
   buffer_byte = 16;
@@ -188,7 +185,7 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
       // iterate over all the bits in the 5bit encoded '1'.
       for (int j = 4; j >= 0; j--)
       {
-        // Extract a single (j) bit from the '1' code, right shift it to the corect position, and write into the buffer.
+        // Extract a single (j) bit from the '1' code, left shift it to the corect position, and write into the buffer.
         ledBuffer[buffer_byte] |= ((LED_ONE >> j) & 0x01) << (buffer_bit % 8);
         // move the buffer bit position we are writting to, a single bit to the right.
         buffer_bit--;
@@ -209,9 +206,7 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
       }
     }
   }
-
-
-  // fill the remaining 4 bytes with encoded zeros
+  // fill the remaining 4 bytes of the 1-wire command with encoded zeros
   ledBuffer[21] = 0x84;
   ledBuffer[22] = 0x21;
 
@@ -234,7 +229,7 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
       // iterate over all the bits in the 5bit encoded '1'.
       for (int j = 4; j >= 0; j--)
       {
-        // Extract a single (j) bit from the '1' code, right shift it to the corect position, and write into the buffer.
+        // Extract a single (j) bit from the '1' code, right left it to the corect position, and write into the buffer.
         ledBuffer[buffer_byte] |= ((LED_ONE >> j) & 0x01) << (buffer_bit % 8);
         // move the buffer bit position we are writting to, a single bit to the right.
         buffer_bit--;
@@ -256,7 +251,7 @@ void set_rgb_led(uint8_t r, uint8_t g, uint8_t b){
     }
   }
 
-  // fill the remaining 4 bytes with encoded zeros
+  // fill the remaining 4 bytes of the 1-wire command with encoded zeros
   ledBuffer[28] |= 0x08;
   ledBuffer[29]  = 0x42;
   ledBuffer[30]  = 0x10;
