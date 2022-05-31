f38cad000a1b558417d4344865a3a0a5a7466ea1
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-05-31 15:24:59 +0200

    03app_dotbot_remote_control: add rtc timeout logic

--- projects/03app_dotbot_remote_control/03app_dotbot_remote_control.c
+++ projects/03app_dotbot_remote_control/03app_dotbot_remote_control.c
@@ -16,14 +16,45 @@
 
 //=========================== defines =========================================
 
-#define BUSY_WAIT(wait_ms) for (int i = 0; i < 3000 * wait_ms; i++) {}      ///< The 3000 magic number approximates to about 1ms.
+#ifndef TIMEOUT_RTC
+#define TIMEOUT_RTC           (NRF_RTC1)
+#endif
+
+#ifndef TIMEOUT_RTC_IRQ
+#define TIMEOUT_RTC_IRQ       (RTC1_IRQn)
+#endif
+
+#ifndef TIMEOUT_RTC_ISR
+#define TIMEOUT_RTC_ISR       (RTC1_IRQHandler)
+#endif
 
 //=========================== variables =========================================
 
 //=========================== main =========================================
 
 static void radio_callback(uint8_t *pkt, uint8_t len) {
+    TIMEOUT_RTC->TASKS_CLEAR = 1; /* Clear RTC counter */
 
+    /* TODO: parse received packet and update the motors' speeds */
+}
+
+static void db_timeout_rtc_init(void) {
+    /* Configure RTC with 10ms delay between ticks */
+    TIMEOUT_RTC->PRESCALER = 327;
+    TIMEOUT_RTC->CC[0] = 10;  /* Fires a compare0 event after 100ms */
+    TIMEOUT_RTC->INTENSET = RTC_INTENSET_COMPARE0_Enabled;
+    NVIC_EnableIRQ(TIMEOUT_RTC_IRQ);
+    /* Start RTC */
+    TIMEOUT_RTC->TASKS_START = 1;
+}
+
+void TIMEOUT_RTC_ISR(void) {
+    if (TIMEOUT_RTC->EVENTS_COMPARE[0]) {
+        NVIC_ClearPendingIRQ(TIMEOUT_RTC_IRQ);
+        TIMEOUT_RTC->EVENTS_COMPARE[0] = 0;
+        /* Stop the motors if the RTC fires a compare events, e.g. when no packet was received during 100ms */
+        db_motors_setSpeed(0, 0);
+    }
 }
 
 /**
@@ -34,8 +65,9 @@ int main(void) {
     db_board_init();
     db_motors_init();
     db_radio_init(&radio_callback);
-    db_radio_set_frequency(8);      // Set the RX frquency to 2408 MHz.
+    db_radio_set_frequency(8);      // Set the RX frequency to 2408 MHz.
     db_radio_rx_enable();           // Start receiving packets.
+    db_timeout_rtc_init();          // Start timeout RTC used to check packet reception
 
     while (1) {
         __WFE(); // Enter a low power state while waiting.
