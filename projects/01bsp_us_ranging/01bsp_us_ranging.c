90b680a7861432fa72286e77f2a3ef04b2a3e3ae
Trifun Savic <trifunsavic@gmail.com>
2022-06-08 17:55:16 +0200

    Created a skeleton for the Ultrasound Ranging code

---
+++ projects/01bsp_us_ranging/01bsp_us_ranging.c
@@ -0,0 +1,32 @@
+/**
+ * @file 01bsp_us_ranging.c
+ * @author Trifun Savic <trifun.savic@inria.fr>
+ * @brief This is a short example of how to do Ultrasound ranging with HC-SR04 sensor on the DotBot board.
+ *
+ * @copyright Inria, 2022
+ *
+ */
+#include <nrf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "board.h"
+#include "us_ranging.h"
+
+//=========================== defines =========================================
+
+//=========================== variables =========================================
+
+//=========================== main =========================================
+
+/**
+ *  @brief The program starts executing here.
+ */
+int main(void) {
+
+    while (1) {
+    }
+
+    // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
+    __NOP();
+}
