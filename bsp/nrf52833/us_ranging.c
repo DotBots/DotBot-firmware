b0f7c96d231092ed7764cd55dfb2f73e8938c037
Trifun Savic <trifunsavic@gmail.com>
2022-06-08 17:55:16 +0200

    Created a skeleton for the Ultrasound Ranging code

---
+++ bsp/nrf52833/us_ranging.c
@@ -0,0 +1,27 @@
+/**
+ * @file us_ranging.c
+ * @addtogroup BSP
+ *
+ * @brief  nRF52833-specific definition of the "us_ranging" bsp module.
+ *
+ * @author Trifun Savic <trifun.savic@inria.fr>
+ *
+ * @copyright Inria, 2022
+ */
+ 
+
+#include <nrf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "us_ranging.h"
+
+//=========================== define ==========================================
+
+
+
+//=========================== variables =======================================
+
+
+
+//=========================== public ==========================================
---
+++ bsp/us_ranging.h
@@ -0,0 +1,22 @@
+#ifndef __US_RANGING_H
+#define __US_RANGING_H
+
+/**
+ * @file us_ranging.h
+ * @addtogroup BSP
+ *
+ * @brief  Cross-platform declaration "us_ranging" bsp module.
+ *
+ * @author Trifun Savic <trifun.savic@inria.fr>
+ *
+ * @copyright Inria, 2022
+ */
+
+#include <nrf.h>
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ //=========================== public ======================================
+ 
+ 
+ #endif
