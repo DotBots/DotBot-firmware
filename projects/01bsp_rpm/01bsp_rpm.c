c6588583971f20a398d39091d6b2db6f85b622a3
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-06-14 10:18:14 +0200

    01bsp_rpm: fix alignment and variable scopes

--- projects/01bsp_rpm/01bsp_rpm.c
+++ projects/01bsp_rpm/01bsp_rpm.c
@@ -31,18 +31,27 @@ int main(void) {
     db_revolution_counter_init();
     db_board_encoder_timers_start();
 
-    uint32_t left_speed, right_speed, left_rpm, right_rpm, left_rps, right_rps = 0;
     while (1) {
-        left_speed = db_board_get_left_speed();
-        left_rpm = db_board_get_left_rpm();
-        left_rps = db_board_get_left_rps();
-        right_speed = db_board_get_right_speed();
-        right_rpm = db_board_get_right_rpm();
-        right_rps = db_board_get_right_rps();
-        printf("Left  - speed: %i, RPM: %i, RPS: %i\n", left_speed, left_rpm, left_rps);
-        printf("Right - speed: %i, RPM: %i, RPS: %i\n", right_speed, right_rpm, right_rps);
-        uint32_t wait = 0x00fffff;
-        while (wait--) {}
+        uint32_t left_speed     = db_board_get_left_speed();
+        uint32_t left_rpm       = db_board_get_left_rpm();
+        uint32_t left_rps       = db_board_get_left_rps();
+        uint32_t right_speed    = db_board_get_right_speed();
+        uint32_t right_rpm      = db_board_get_right_rpm();
+        uint32_t right_rps      = db_board_get_right_rps();
+        uint32_t wait           = 0x00fffff;
+        printf(
+            "Left  - speed: %i, RPM: %i, RPS: %i\n",
+            left_speed,
+            left_rpm,
+            left_rps
+        );
+        printf(
+            "Right - speed: %i, RPM: %i, RPS: %i\n",
+            right_speed,
+            right_rpm,
+            right_rps
+        );
+        while (wait--);
     }
 
     // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
