da31b3fd5dfc845c18a64a7c4cca7f909ca488ff
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-06-28 15:36:50 +0200

    bsp/board: drop DB_PIN_* macros + cleanup includes

--- bsp/board.h
+++ bsp/board.h
@@ -12,20 +12,6 @@
  * @copyright Inria, 2022
  */
 
-#include <nrf.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-//=========================== defines ==========================================
-
-/**
- * Enable a pin as output, must be called once before using the other macros below
- */
-#define DB_PIN_ENABLE(port, pin)    NRF_P##port->DIRSET = (1 << pin)
-#define DB_PIN_ON(port, pin)        NRF_P##port->OUTSET = (1 << pin)    /**< Turn on the pin */
-#define DB_PIN_OFF(port, pin)       NRF_P##port->OUTCLR = (1 << pin)    /**< Turn off the pin */
-#define DB_PIN_TOGGLE(port, pin)    NRF_P##port->OUT ^= (1 << pin)      /**< Toggle the pin */
-
 //=========================== public ===========================================
 
 void db_board_init(void);
