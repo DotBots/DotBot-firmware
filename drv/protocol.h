da18a26d34bed404b4cf761653296e090ff98652
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-07-18 11:27:56 +0200

    drv/command: apply review comments

--- drv/protocol.h
+++ drv/protocol.h
@@ -1,11 +1,11 @@
-#ifndef __COMMAND_H
-#define __COMMAND_H
+#ifndef __PROTOCOL_H
+#define __PROTOCOL_H
 
 /**
- * @file command.h
+ * @file procotol.h
  * @addtogroup BSP
  *
- * @brief  Cross-platform declaration "command" bsp module.
+ * @brief  Cross-platform declaration "protocol" driver module.
  *
  * @author Alexandre Abadie <alexandre.abadie@inria.fr>
  *
@@ -16,29 +16,34 @@
 
 //=========================== defines ==========================================
 
-#define DB_COMMAND_VERSION      (0)     ///< Version of the command protocol
+#define DB_PROTOCOL_VERSION     (0)     ///< Version of the command protocol
 
 typedef enum {
-    DB_COMMAND_MOVE_RAW = 0,            ///< Move raw command type
-    DB_COMMAND_RGB_LED  = 1,            ///< RGB LED command type
+    DB_PROTOCOL_CMD_MOVE_RAW    = 0,    ///< Move raw command type
+    DB_PROTOCOL_CMD_RGB_LED     = 1,    ///< RGB LED command type
 } command_type_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t version;                    ///< Version of the protocol
+    command_type_t type;                ///< Type of command following this header
+} protocol_header_ht;
 
 typedef struct __attribute__((packed)) {
     int8_t left_x;                      ///< Horizontal coordinate for left side
     int8_t left_y;                      ///< Vertical coordinate for left side
     int8_t right_x;                     ///< Horizontal coordinate for right side
     int8_t right_y;                     ///< Vertical coordinate for right side
-} move_raw_command_t;
+} protocol_move_raw_command_ht;
 
 typedef struct __attribute__((packed)) {
     uint8_t r;                          ///< Red component value
     uint8_t g;                          ///< Green component value
     uint8_t b;                          ///< Blue component value
-} rgbled_command_t;
+} protocol_rgbled_command_ht;
 
 //=========================== public ===========================================
 
-void db_command_move_raw_to_buffer(uint8_t *buffer, move_raw_command_t *command);
-void db_command_rgbled_to_buffer(uint8_t *buffer, rgbled_command_t *command);
+void db_protocol_cmd_move_raw_to_buffer(uint8_t *buffer, protocol_move_raw_command_ht *command);
+void db_protocol_cmd_rgbled_to_buffer(uint8_t *buffer, protocol_rgbled_command_ht *command);
 
 #endif
