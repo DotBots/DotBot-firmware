b18e4ccfceb79754dcb866477840bf1216b6239e
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-07-18 15:22:46 +0200

    03app_dotbot_joystick: update with new protocol API

--- projects/03app_dotbot_joystick/03app_dotbot_joystick.c
+++ projects/03app_dotbot_joystick/03app_dotbot_joystick.c
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 // Include BSP headers
 #include "board.h"
-#include "command.h"
+#include "protocol.h"
 #include "motors.h"
 #include "radio.h"
 #include "rgbled.h"
@@ -27,25 +27,29 @@ static uint32_t _last_packet_received = 0;
 
 static void radio_callback(uint8_t *pkt, uint8_t len) {
     _last_packet_received = db_timer_hf_now();
+    uint8_t * const ptk_ptr = pkt;
+    protocol_header_ht *header = (protocol_header_ht *)ptk_ptr;
     // Check version is supported
-    if (pkt[0] != DB_COMMAND_VERSION) {
+    if (header->version != DB_PROTOCOL_VERSION) {
+        printf("Invalid version '%d', expected '%d'\n", header->version, DB_PROTOCOL_VERSION);
         return;
     }
 
+    uint8_t * const cmd_ptr = ptk_ptr + sizeof(protocol_header_ht);
     // parse received packet and update the motors' speeds
-    switch (pkt[1]) {
-        case DB_COMMAND_MOVE_RAW:
+    switch (header->type) {
+        case DB_PROTOCOL_CMD_MOVE_RAW:
         {
-            move_raw_command_t *command = (move_raw_command_t *)&pkt[2];
+            protocol_move_raw_command_ht *command = (protocol_move_raw_command_ht *)cmd_ptr;
             int16_t left = (int16_t)(100 * ((float)command->left_y / INT8_MAX));
             int16_t right = (int16_t)(100 * ((float)command->right_y / INT8_MAX));
             printf("Move: %i-%i\n", left, right);
             db_motors_set_speed(left, right);
         }
             break;
-        case DB_COMMAND_RGB_LED:
+        case DB_PROTOCOL_CMD_RGB_LED:
         {
-            rgbled_command_t *command = (rgbled_command_t *)&pkt[2];
+            protocol_rgbled_command_ht *command = (protocol_rgbled_command_ht *)cmd_ptr;
             printf("%d-%d-%d\n", command->r, command->g, command->b
             );
             db_rgbled_set(command->r, command->g, command->b);
