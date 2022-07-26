62301bf5acd234182fb61329f2f5ca401e1cb30f
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-07-26 12:51:53 +0200

    projects/03app_dotbot_{gateway/joystick}: apply clang-format

--- projects/03app_dotbot_gateway/03app_dotbot_gateway.c
+++ projects/03app_dotbot_gateway/03app_dotbot_gateway.c
@@ -19,11 +19,11 @@
 
 //=========================== defines ==========================================
 
-#define DB_UART_MAX_BYTES   (32U)   ///< Max bytes in UART receive buffer
+#define DB_UART_MAX_BYTES (32U)  ///< Max bytes in UART receive buffer
 
 typedef enum {
-    UART_STATE_IDLE = 0,            ///< The UART is ready to start receiving messages
-    UART_STATE_RECEIVING,           ///< The UART is receiving messages
+    UART_STATE_IDLE = 0,   ///< The UART is ready to start receiving messages
+    UART_STATE_RECEIVING,  ///< The UART is receiving messages
 } uart_state_t;
 
 typedef struct {
@@ -32,36 +32,36 @@ typedef struct {
 } uart_message_t;
 
 typedef struct {
-    uart_message_t message;         ///< Structure that handles the UART message
-    uart_state_t state;             ///< Internal state of the UART (idle or receiving)
-    uint8_t expected_length;        ///< Expected length of message to receive
+    uart_message_t message;   ///< Structure that handles the UART message
+    uart_state_t state;       ///< Internal state of the UART (idle or receiving)
+    uint8_t expected_length;  ///< Expected length of message to receive
 } uart_ctx_t;
 
 //=========================== variables ========================================
 
-static uart_ctx_t _uart_ctx;        // Variable handling the UART context
-static const gpio_t _rx_pin = { .pin = 8, .port = 0};
-static const gpio_t _tx_pin = { .pin = 6, .port = 0};
+static uart_ctx_t _uart_ctx;  // Variable handling the UART context
+static const gpio_t _rx_pin = { .pin = 8, .port = 0 };
+static const gpio_t _tx_pin = { .pin = 6, .port = 0 };
 
 //=========================== callbacks ========================================
 
 static void uart_callback(uint8_t data) {
     switch (_uart_ctx.state) {
-        case UART_STATE_IDLE:
-            _uart_ctx.expected_length = data;
-            _uart_ctx.message.pos = 0;
-            _uart_ctx.state = UART_STATE_RECEIVING;
-            break;
-        case UART_STATE_RECEIVING:
-            _uart_ctx.message.buffer[_uart_ctx.message.pos] = data;
-            if (_uart_ctx.message.pos == _uart_ctx.expected_length - 1 || _uart_ctx.message.pos == DB_UART_MAX_BYTES - 1) {
-                db_radio_tx(_uart_ctx.message.buffer, _uart_ctx.expected_length);
-                _uart_ctx.state = UART_STATE_IDLE;
-            }
-            _uart_ctx.message.pos++;
-            break;
-        default:
-            break;
+    case UART_STATE_IDLE:
+        _uart_ctx.expected_length = data;
+        _uart_ctx.message.pos     = 0;
+        _uart_ctx.state           = UART_STATE_RECEIVING;
+        break;
+    case UART_STATE_RECEIVING:
+        _uart_ctx.message.buffer[_uart_ctx.message.pos] = data;
+        if (_uart_ctx.message.pos == _uart_ctx.expected_length - 1 || _uart_ctx.message.pos == DB_UART_MAX_BYTES - 1) {
+            db_radio_tx(_uart_ctx.message.buffer, _uart_ctx.expected_length);
+            _uart_ctx.state = UART_STATE_IDLE;
+        }
+        _uart_ctx.message.pos++;
+        break;
+    default:
+        break;
     }
 }
 
@@ -78,15 +78,15 @@ int main(void) {
     NRF_P0->DIRSET = 1 << 13;
     NRF_P0->OUTCLR = 1 << 13;
     // Configure Radio as transmitter
-    db_radio_init(NULL); // Set the callback function.
-    db_radio_set_frequency(8);      // Set the radio frequency to 2408 MHz.
+    db_radio_init(NULL);        // Set the callback function.
+    db_radio_set_frequency(8);  // Set the radio frequency to 2408 MHz.
     // Initialize the uart context
     _uart_ctx.expected_length = 0;
-    _uart_ctx.state = UART_STATE_IDLE;
+    _uart_ctx.state           = UART_STATE_IDLE;
     db_uart_init(&_rx_pin, &_tx_pin, &uart_callback);
 
     while (1) {
-       __WFE();
+        __WFE();
     }
 
     // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
