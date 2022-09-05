a13c47da615959323822ce775bccacce7f47fcb7
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-09-05 10:49:54 +0200

    projects/03app_dotbot: apply review comments

--- projects/03app_dotbot/03app_dotbot.c
+++ projects/03app_dotbot/03app_dotbot.c
@@ -1,14 +1,16 @@
 /**
- * @file 03app_dotbot_remote_control.c
+ * @file 03app_dotbot.c
  * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
  * @author Alexandre Abadie <alexandre.abadie@inria.fr>
- * @brief This application is used to control a single dotbot remotely with a joystick using radio.
+ * @brief This application is used to control a single dotbot remotely
+ *
+ * The remote control can be either a keyboard, a joystick or buttons on the gateway
+ * itself
  *
  * @copyright Inria, 2022
  *
  */
 #include <nrf.h>
-#include <stdio.h>
 #include <stdlib.h>
 // Include BSP headers
 #include "board.h"
@@ -18,55 +20,57 @@
 #include "rgbled.h"
 #include "timer_hf.h"
 
-//=========================== defines =========================================
+//=========================== defines ==========================================
 
-#define MAX_RECEIVE_DELAY_US (100 * 1000)
+#define TIMEOUT_CHECK_DELAY_US (100 * 1000)  ///< 100 ms delay between packet received timeout checks
 
-static uint32_t _last_packet_received = 0;
+typedef struct {
+    uint32_t ts_last_packet_received;  ///< Last timestamp in microseconds a control packet was received
+} dotbot_vars_t;
 
-//=========================== main =========================================
+//=========================== variables ========================================
+
+static dotbot_vars_t _dotbot_vars;
+
+//=========================== prototypes =======================================
+
+static void _timeout_check(void);
+
+//=========================== callbacks ========================================
 
 static void radio_callback(uint8_t *pkt, uint8_t len) {
-    _last_packet_received      = db_timer_hf_now();
-    uint8_t *const ptk_ptr     = pkt;
-    protocol_header_ht *header = (protocol_header_ht *)ptk_ptr;
+    _dotbot_vars.ts_last_packet_received = db_timer_hf_now();
+    uint8_t *ptk_ptr                     = pkt;
+    protocol_header_ht *header           = (protocol_header_ht *)ptk_ptr;
     // Check version is supported
     if (header->version != DB_PROTOCOL_VERSION) {
-        printf("Invalid version '%d', expected '%d'\n", header->version, DB_PROTOCOL_VERSION);
         return;
     }
 
-    uint8_t *const cmd_ptr = ptk_ptr + sizeof(protocol_header_ht);
+    uint8_t *cmd_ptr = ptk_ptr + sizeof(protocol_header_ht);
     // parse received packet and update the motors' speeds
     switch (header->type) {
-    case DB_PROTOCOL_CMD_MOVE_RAW: {
-        protocol_move_raw_command_ht *command = (protocol_move_raw_command_ht *)cmd_ptr;
-        int16_t left                          = (int16_t)(100 * ((float)command->left_y / INT8_MAX));
-        int16_t right                         = (int16_t)(100 * ((float)command->right_y / INT8_MAX));
-        printf("Move: %i-%i\n", left, right);
-        db_motors_set_speed(left, right);
-    } break;
-    case DB_PROTOCOL_CMD_RGB_LED: {
-        protocol_rgbled_command_ht *command = (protocol_rgbled_command_ht *)cmd_ptr;
-        printf("%d-%d-%d\n", command->r, command->g, command->b);
-        db_rgbled_set(command->r, command->g, command->b);
-    } break;
+        case DB_PROTOCOL_CMD_MOVE_RAW:
+        {
+            protocol_move_raw_command_ht *command = (protocol_move_raw_command_ht *)cmd_ptr;
+            int16_t left                          = (int16_t)(100 * ((float)command->left_y / INT8_MAX));
+            int16_t right                         = (int16_t)(100 * ((float)command->right_y / INT8_MAX));
+            db_motors_set_speed(left, right);
+        } break;
+        case DB_PROTOCOL_CMD_RGB_LED:
+        {
+            protocol_rgbled_command_ht *command = (protocol_rgbled_command_ht *)cmd_ptr;
+            db_rgbled_set(command->r, command->g, command->b);
+        } break;
     }
 }
 
-static void timeout_check(void) {
-    uint32_t now = db_timer_hf_now();
-    if (now > _last_packet_received + MAX_RECEIVE_DELAY_US) {
-        db_motors_set_speed(0, 0);
-        puts("stopping motors");
-    }
-}
+//=========================== main =============================================
 
 /**
  *  @brief The program starts executing here.
  */
 int main(void) {
-    puts("DotBot application");
     db_board_init();
     db_rgbled_init();
     db_motors_init();
@@ -74,7 +78,7 @@ int main(void) {
     db_radio_set_frequency(8);  // Set the RX frequency to 2408 MHz.
     db_radio_rx_enable();       // Start receiving packets.
     db_timer_hf_init();
-    db_timer_hf_set_periodic_us(0, MAX_RECEIVE_DELAY_US, &timeout_check);
+    db_timer_hf_set_periodic_us(0, TIMEOUT_CHECK_DELAY_US, &_timeout_check);
 
     while (1) {
         __WFE();  // Enter a low power state while waiting.
@@ -82,4 +86,13 @@ int main(void) {
 
     // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
     __NOP();
+}
+
+//=========================== private functions ================================
+
+static void _timeout_check(void) {
+    uint32_t now = db_timer_hf_now();
+    if (now > _dotbot_vars.ts_last_packet_received + TIMEOUT_CHECK_DELAY_US) {
+        db_motors_set_speed(0, 0);
+    }
 }
--- projects/03app_dotbot/README.md
+++ projects/03app_dotbot/README.md
@@ -4,7 +4,7 @@ This application allows the DotBot to be controlled remotely either from
 - a joystick or nrf52 compatible board running a firmware that sends compatible
   commands _move_ or _rgbled_
 - a computer running [the bot-controller tool](https://github.com/DotBot/Botcontroller-python)
-and with a nRF52840DK connected to it and used as gateway. The nRF52840DK must run the
+and with a nRF52840-DK connected to it and used as gateway. The nRF52840-DK must run the
 `03app_dotbot_gateway` firmware.
 
 <!--
