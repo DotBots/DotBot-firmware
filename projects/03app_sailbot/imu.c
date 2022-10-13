78350b38fb82f63f472ec305d648154f9172f911
Mališa Vučinić <malishav@gmail.com>
2022-10-13 18:37:56 +0200

    imu: Add files

---
+++ projects/03app_sailbot/imu.c
@@ -0,0 +1,8 @@
+/**
+ * @file imu.c
+ * @author Mališa Vučinić <malisa.vucinic@inria.fr>
+ * @brief Module for controlling the IMU on Kyosho Fortune 612 SailBot.
+ *
+ * @copyright Inria, 2022
+ *
+ */
---
+++ projects/03app_sailbot/imu.h
@@ -0,0 +1,19 @@
+#ifndef __IMU_H
+#define __IMU_H
+
+/**
+ * @file imu.h
+ * @addtogroup sailbot
+ *
+ * @brief  Module for controlling the IMU on the SailBot.
+ *
+ * @author Mališa Vučinić <malisa.vucinic@inria.fr>
+ *
+ * @copyright Inria, 2022
+ */
+#include <stdint.h>
+#include <stdlib.h>
+#include <nrf.h>
+
+
+#endif
