b050bb1af3a7779307397cab3030137cb46d3022
Mališa Vučinić <malishav@gmail.com>
2022-10-18 14:49:32 +0200

    imu: Use assert for self-test

--- projects/03app_sailbot/imu.c
+++ projects/03app_sailbot/imu.c
@@ -8,6 +8,8 @@
  */
 
 #include "gpio.h"
+#include "assert.h"
+#include "i2c.h"
 
 static const gpio_t scl = { .port = 1, .pin = 9 };
 static const gpio_t sda = { .port = 0, .pin = 11 };
@@ -15,19 +17,19 @@ static const gpio_t sda = { .port = 0, .pin = 11 };
 #define ICM20948_ADDR         (0x68)
 #define ICM20948_WHO_AM_I_REG (0x00)
 #define ICM20948_WHO_AM_I_VAL (0xEA)
+#define ICM20948_BANK_SEL_REG (0x7f)
+
+#define AK09916_ADDR (0x0C)
+#define AK09916_WIA1_REG (0x00)
+#define AK09916_WIA1_VAL (0x48)
 
 void imu_init(void) {
+    uint8_t who_am_i;
 
     db_i2c_init(&scl, &sda);
     db_i2c_begin();
-    uint8_t who_am_i;
     db_i2c_read_regs(ICM20948_ADDR, ICM20948_WHO_AM_I_REG, &who_am_i, 1);
-    if (who_am_i != ICM20948_WHO_AM_I_VAL) {
-        printf(
-            "Invalid WHO_AM_I value '%d' (expected: %d)\n",
-            who_am_i, ICM20948_WHO_AM_I_VAL);
-        return;
-    } else {
-        printf("Valid WHO_AM_I value\n");
-    }
+    assert(who_am_i == ICM20948_WHO_AM_I_VAL);
+
+    db_i2c_end();
 }
