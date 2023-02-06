f43faa6c15d2f35b2a41c39ebbb20a095f01a8f2
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-02-06 15:16:54 +0100

    bsp: add API for interacting with GPIOs

--- bsp/gpio.h
+++ bsp/gpio.h
@@ -9,7 +9,7 @@
  *
  * @author Alexandre Abadie <alexandre.abadie@inria.fr>
  *
- * @copyright Inria, 2022
+ * @copyright Inria, 2022-2023
  */
 
 #include <nrf.h>
@@ -20,10 +20,25 @@
 /**
  * Enable a pin as output, must be called once before using the other macros below
  */
-#define DB_GPIO_ENABLE(port, pin) NRF_P##port->DIRSET = (1 << pin)
+#define DB_GPIO_ENABLE(port, pin) NRF_P##port->DIRSET = (1 << pin)  ///< Enable a pin as output
 #define DB_GPIO_ON(port, pin)     NRF_P##port->OUTSET = (1 << pin)  ///< Turn on the pin
 #define DB_GPIO_OFF(port, pin)    NRF_P##port->OUTCLR = (1 << pin)  ///< Turn off the pin
 #define DB_GPIO_TOGGLE(port, pin) NRF_P##port->OUT ^= (1 << pin)    ///< Toggle the pin
+
+typedef void (*gpio_cb_t)(void *ctx);  ///< Callback function prototype, it is called on each gpio interrupt
+
+typedef enum {
+    DB_GPIO_OUT,    ///< Floating output
+    DB_GPIO_IN,     ///< Floating input
+    DB_GPIO_IN_PU,  ///< Pull up input
+    DB_GPIO_IN_PD,  ///< Pull down input
+} gpio_mode_t;
+
+typedef enum {
+    DB_GPIO_IRQ_EDGE_RISING  = GPIOTE_CONFIG_POLARITY_LoToHi,  ///< Rising edge
+    DB_GPIO_IRQ_EDGE_FALLING = GPIOTE_CONFIG_POLARITY_HiToLo,  ///< Falling edge
+    DB_GPIO_IRQ_EDGE_BOTH    = GPIOTE_CONFIG_POLARITY_Toggle,  ///< Both falling and rising edges
+} gpio_irq_edge_t;
 
 typedef struct {
     uint8_t port;  ///< Port number of the GPIO
@@ -33,5 +48,55 @@ typedef struct {
 //=========================== variables ========================================
 
 static NRF_GPIO_Type *nrf_port[2] = { NRF_P0, NRF_P1 };
+
+//============================ public ==========================================
+
+/**
+ * @brief   Initialize a GPIO
+ *
+ * @param[in]   gpio            Pointer to the GPIO descriptor
+ * @param[in]   mode            GPIO mode (output, input, etc)
+ */
+void db_gpio_init(const gpio_t *gpio, gpio_mode_t mode);
+
+/**
+ * @brief   Initialize a GPIO with IRQ
+ *
+ * @param[in]   gpio            Pointer to the GPIO descriptor
+ * @param[in]   mode            GPIO mode, should one flavor of input
+ * @param[in]   edge            Type of edge to trigger the IRQ (Falling, Rising or both)
+ * @param[in]   callback        Function pointer that is called from gpio ISR
+ * @param[in]   ctx             Pointer to some context passed as parameter to the callback
+ */
+void db_gpio_init_irq(const gpio_t *gpio, gpio_mode_t mode, gpio_irq_edge_t edge, gpio_cb_t callback, void *ctx);
+
+/**
+ * @brief   Set the GPIO output high
+ *
+ * @param[in]   gpio            Pointer to the GPIO descriptor
+ */
+void db_gpio_set(const gpio_t *gpio);
+
+/**
+ * @brief   Set the GPIO output low
+ *
+ * @param[in]   gpio            Pointer to the GPIO descriptor
+ */
+void db_gpio_clear(const gpio_t *gpio);
+
+/**
+ * @brief   Toggle the GPIO output
+ *
+ * @param[in]   gpio            Pointer to the GPIO descriptor
+ */
+void db_gpio_toggle(const gpio_t *gpio);
+
+/**
+ * @brief   Read the value of a GPIO
+ *
+ * @param[in]   gpio            Pointer to the GPIO descriptor
+ * @return                      GPIO level (0 or 1)
+ */
+uint8_t db_gpio_read(const gpio_t *gpio);
 
 #endif
---
+++ bsp/nrf52833/gpio.c
@@ -0,0 +1,111 @@
+/**
+ * @file gpio.c
+ * @addtogroup BSP
+ *
+ * @brief  nRF52833-specific definition of the "gpio" bsp module.
+ *
+ * @author Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ * @copyright Inria, 2023
+ */
+#include <nrf.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "gpio.h"
+
+//=========================== defines ==========================================
+
+#define GPIOTE_MAX_CHANNELS (8U)
+
+typedef struct {
+    gpio_cb_t callback;
+    void     *ctx;
+} gpio_event_t;
+
+typedef struct {
+    uint8_t       gpiote_pin_idx;
+    gpio_t const *gpiote_pins[GPIOTE_MAX_CHANNELS];
+    gpio_event_t  events[GPIOTE_MAX_CHANNELS];
+} gpio_vars_t;
+
+//=========================== variables ========================================
+
+static gpio_vars_t _gpio_vars;
+
+//=========================== public ===========================================
+
+void db_gpio_init(const gpio_t *gpio, gpio_mode_t mode) {
+
+    switch (mode) {
+        case DB_GPIO_OUT:
+            nrf_port[gpio->port]->PIN_CNF[gpio->pin] |= GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos;
+            break;
+        case DB_GPIO_IN:
+            nrf_port[gpio->port]->PIN_CNF[gpio->pin] &= ~(1UL << GPIO_PIN_CNF_INPUT_Pos);
+            break;
+        case DB_GPIO_IN_PU:
+            nrf_port[gpio->port]->PIN_CNF[gpio->pin] |= (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos);
+            nrf_port[gpio->port]->PIN_CNF[gpio->pin] &= ~(1UL << GPIO_PIN_CNF_INPUT_Pos);
+            break;
+        case DB_GPIO_IN_PD:
+            nrf_port[gpio->port]->PIN_CNF[gpio->pin] |= (GPIO_PIN_CNF_PULL_Pulldown << GPIO_PIN_CNF_PULL_Pos);
+            nrf_port[gpio->port]->PIN_CNF[gpio->pin] &= ~(1UL << GPIO_PIN_CNF_INPUT_Pos);
+            break;
+    }
+}
+
+void db_gpio_init_irq(const gpio_t *gpio, gpio_mode_t mode, gpio_irq_edge_t edge, gpio_cb_t callback, void *ctx) {
+    uint8_t gpio_pin_idx = 0xff;
+    for (uint8_t i = 0; i < _gpio_vars.gpiote_pin_idx; i++) {
+        if (_gpio_vars.gpiote_pins[i] == gpio) {
+            gpio_pin_idx = i;
+        }
+    }
+    if (gpio_pin_idx == 0xff) {
+        assert(_gpio_vars.gpiote_pin_idx < GPIOTE_MAX_CHANNELS);
+        gpio_pin_idx                         = _gpio_vars.gpiote_pin_idx;
+        _gpio_vars.gpiote_pins[gpio_pin_idx] = gpio;
+        _gpio_vars.gpiote_pin_idx++;
+    }
+
+    _gpio_vars.events[gpio_pin_idx].callback = callback;
+    _gpio_vars.events[gpio_pin_idx].ctx      = ctx;
+
+    db_gpio_init(gpio, mode);
+    NVIC_EnableIRQ(GPIOTE_IRQn);
+
+    NRF_GPIOTE->CONFIG[gpio_pin_idx] = (GPIOTE_CONFIG_MODE_Event << GPIOTE_CONFIG_MODE_Pos) |
+                                       (gpio->pin << GPIOTE_CONFIG_PSEL_Pos) |
+                                       (edge << GPIOTE_CONFIG_POLARITY_Pos);
+    NRF_GPIOTE->INTENSET |= (GPIOTE_INTENSET_IN0_Msk << gpio_pin_idx);
+}
+
+void db_gpio_set(const gpio_t *gpio) {
+    nrf_port[gpio->port]->OUTSET = (1 << gpio->pin);
+}
+
+void db_gpio_clear(const gpio_t *gpio) {
+    nrf_port[gpio->port]->OUTCLR = (1 << gpio->pin);
+}
+
+void db_gpio_toggle(const gpio_t *gpio) {
+    nrf_port[gpio->port]->OUT ^= (1 << gpio->pin);
+}
+
+uint8_t db_gpio_read(const gpio_t *gpio) {
+    if (nrf_port[gpio->port]->DIR & (1 << gpio->pin)) {
+        return (nrf_port[gpio->port]->OUT & (1 << gpio->pin)) ? 1 : 0;
+    } else {
+        return (nrf_port[gpio->port]->IN & (1 << gpio->pin)) ? 1 : 0;
+    }
+}
+
+void GPIOTE_IRQHandler(void) {
+    for (uint8_t i = 0; i < _gpio_vars.gpiote_pin_idx; ++i) {
+        if (NRF_GPIOTE->EVENTS_IN[i] == 1) {
+            NRF_GPIOTE->EVENTS_IN[i] = 0;
+            _gpio_vars.events[i].callback(_gpio_vars.events[i].ctx);
+            break;
+        }
+    }
+}
