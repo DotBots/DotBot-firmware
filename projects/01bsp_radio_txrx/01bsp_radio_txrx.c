49f731130140d8e459e99e80880084ed786ad55c
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-03-25 22:50:44 +0100

    projects/01bsp_radio_txrx: improve test application

--- projects/01bsp_radio_txrx/01bsp_radio_txrx.c
+++ projects/01bsp_radio_txrx/01bsp_radio_txrx.c
@@ -3,11 +3,6 @@
  * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
  * @brief This is a short example of how to interface with the onboard Radio in the DotBot board.
  *
- * This program is the RX part.
- * if the corresponding compatible TX program is loaded into a nearby nRF52840-DK,
- * pressing the buttons in the DK should toggle ON and OFF the P0.31 pin on the DotBot (P0.31).
- *
- *
  * @copyright Inria, 2022
  *
  */
@@ -18,20 +13,27 @@
 #include "board.h"
 #include "gpio.h"
 #include "radio.h"
-
-//=========================== defines ===========================================
-
-#define NUMBER_OF_BYTES_IN_PACKET 32
+#include "timer.h"
 
 //=========================== variables =========================================
 
-static uint8_t packet_tx[NUMBER_OF_BYTES_IN_PACKET];
+static uint8_t packet_tx[] = {
+    0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48,
+    0x49, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x00
+};
 
-static const gpio_t _dbg_pin = { .port = 0, .pin = 31 };
+#if defined(NRF5340_XXAA)
+static const gpio_t _dbg_pin = { .port = 0, .pin = 28 };
+#else
+static const gpio_t _dbg_pin = { .port = 0, .pin = 13 };
+#endif
 
-//=========================== prototypes ========================================
+//=========================== functions =========================================
 
-void radio_callback(uint8_t *packet, uint8_t length);
+static void radio_callback(uint8_t *packet, uint8_t length) {
+    printf("packet received (%dB): %s\n", length, (char *)packet);
+    db_gpio_toggle(&_dbg_pin);
+}
 
 //=========================== main ==============================================
 
@@ -43,48 +45,23 @@ int main(void) {
     // Turn ON the DotBot board regulator
     db_board_init();
 
-    //=========================== Initialize GPIO =========================================
+    //=========================== Initialize GPIO and timer =====================
 
     db_gpio_init(&_dbg_pin, DB_GPIO_OUT);
+    db_timer_init();
 
-    //=========================== Configure Radio =========================================
+    //=========================== Configure Radio ===============================
 
     db_radio_init(&radio_callback, DB_RADIO_BLE_1MBit);
     db_radio_set_frequency(8);  // Set the RX frquency to 2408 MHz.
 
-    db_radio_tx(packet_tx, NUMBER_OF_BYTES_IN_PACKET);
-    db_radio_rx_enable();  // Start receiving packets.
-
     while (1) {
-
-        __WFE();  // Enter a low power state while waiting.
+        db_radio_rx_disable();
+        db_radio_tx(packet_tx, sizeof(packet_tx) / sizeof(packet_tx[0]));
+        db_radio_rx_enable();
+        db_timer_delay_ms(100);
     }
 
     // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
     __NOP();
-}
-
-//=========================== functions =========================================
-/**
- *  @brief Callback function to process received packets
- *
- * This function gets called each time a packet is received.
- *
- * @param[in] packet pointer to the array of data to send over the radio (max size = 32)
- * @param[in] length Number of bytes to send (max size = 32)
- *
- */
-void radio_callback(uint8_t *packet, uint8_t length) {
-    (void)length;
-
-    // Check the arriving packet for any pressed button.
-    if (packet[0] == 0x01 || packet[1] == 0x01 || packet[2] == 0x01 || packet[3] == 0x01) {
-
-        db_gpio_set(&_dbg_pin);
-    }
-
-    else {
-
-        db_gpio_clear(&_dbg_pin);
-    }
 }
