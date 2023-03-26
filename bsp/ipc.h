35f126a6daf55308ba22b25c3f5d33aea170d4b2
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-03-26 18:32:20 +0200

    bsp/ipc: add inline documentation

--- bsp/ipc.h
+++ bsp/ipc.h
@@ -34,7 +34,6 @@ typedef enum {
     DB_IPC_RADIO_ADDR_REQ,    ///< Request for radio set network address
     DB_IPC_RADIO_ADDR_ACK,    ///< Acknowledment for radio set network address
     DB_IPC_RADIO_RX_REQ,      ///< Request for radio rx
-    DB_IPC_RADIO_RX_ACK,      ///< Acknowledment for radio rx
     DB_IPC_RADIO_RX_EN_REQ,   ///< Request for radio rx enable
     DB_IPC_RADIO_RX_EN_ACK,   ///< Acknowledment for radio rx enable
     DB_IPC_RADIO_RX_DIS_REQ,  ///< Request for radio rx disable
@@ -48,53 +47,62 @@ typedef enum {
 } ipc_event_type_t;
 
 typedef struct {
-    db_radio_ble_mode_t mode;
+    db_radio_ble_mode_t mode;  ///< Radio mode (2Mbit/s, 1Mbit/s, LR500kbit/s, LR125kbit/s
 } ipc_radio_init_param_t;
 
 typedef struct {
-    uint8_t frequency;
+    uint8_t frequency;  ///< Radio frequency
 } ipc_radio_set_freq_param_t;
 
 typedef struct {
-    uint8_t channel;
+    uint8_t channel;  ///< Radio channel
 } ipc_radio_set_chan_param_t;
 
 typedef struct {
     uint32_t addr;
-} ipc_radio_set_addr_param_t;
+} ipc_radio_set_addr_param_t;  ///< Network address used to configure the radio
 
 typedef struct {
-    uint8_t length;
-    uint8_t buffer[UINT8_MAX];
+    uint8_t length;             ///< Length of the pdu in bytes
+    uint8_t buffer[UINT8_MAX];  ///< Buffer containing the pdu data
 } ipc_radio_pdu_t;
 
 typedef struct {
-    ipc_radio_init_param_t     init_param;
-    ipc_radio_set_freq_param_t freq_param;
-    ipc_radio_set_chan_param_t chan_param;
-    ipc_radio_set_addr_param_t addr_param;
-    ipc_radio_pdu_t            tx_param;
-    ipc_radio_pdu_t            rx_param;
+    ipc_radio_init_param_t     init_param;  ///< db_radio_init function parameters
+    ipc_radio_set_freq_param_t freq_param;  ///< db_set_frequency function parameters
+    ipc_radio_set_chan_param_t chan_param;  ///< db_set_channel function parameters
+    ipc_radio_set_addr_param_t addr_param;  ///< db_set_network_address function parameters
+    ipc_radio_pdu_t            tx_param;    ///< PDU to send
+    ipc_radio_pdu_t            rx_param;    ///< Received pdu
 } ipc_radio_data_t;
 
 typedef struct {
-    uint8_t value;
+    uint8_t value;  ///< Byte containing the random value read
 } ipc_rng_data_t;
 
 typedef struct __attribute__((packed)) {
-    ipc_event_type_t event;
-    ipc_radio_data_t radio;
-    ipc_rng_data_t   rng;
+    ipc_event_type_t event;  ///< IPC event
+    ipc_radio_data_t radio;  ///< Radio shared data
+    ipc_rng_data_t   rng;    ///< Rng share data
 } ipc_shared_data_t;
 
+/**
+ * @brief Lock the mutex, blocks until the mutex is locked
+ */
 static inline void mutex_lock(void) {
     while (NRF_MUTEX->MUTEX[0]) {}
 }
 
+/**
+ * @brief Unlock the mutex, has no effect if the mutex is already unlocked
+ */
 static inline void mutex_unlock(void) {
     NRF_MUTEX->MUTEX[0] = 0;
 }
 
+/**
+ * @brief Variable in RAM containing the shared data structure
+ */
 volatile __attribute__((section(".ARM.__at_0x20004000"))) ipc_shared_data_t ipc_shared_data;
 
 #endif
