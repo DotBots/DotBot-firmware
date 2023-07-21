72d782e4fc562105194adf3d9d68620ffe7215f2
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-07-21 09:58:23 +0200

    bsp/pwm: refactor to support multiple PWM peripherals

--- bsp/nrf/pwm.c
+++ bsp/nrf/pwm.c
@@ -14,17 +14,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <nrf.h>
+#include <nrf_peripherals.h>
 
 #include "gpio.h"
 #include "pwm.h"
 
 //=========================== defines ==========================================
 
-#if defined(NRF5340_XXAA) && defined(NRF_APPLICATION)
-#define DB_PWM (NRF_PWM0_S)
-#else
-#define DB_PWM (NRF_PWM0)
-#endif
 #define DB_PWM_MAX_CHANNELS (4)
 
 // Variable that stores the PWM duty cycle for all four PWM channels
@@ -35,43 +31,56 @@ typedef struct {
 
 //=========================== variables ========================================
 
+static NRF_PWM_Type *_pwm_devs[PWM_COUNT] = {
+#if defined(NRF5340_XXAA) && defined(NRF_APPLICATION)
+    NRF_PWM0_S,
+    NRF_PWM1_S,
+    NRF_PWM2_S,
+    NRF_PWM3_S,
+#else
+    NRF_PWM0,
+    NRF_PWM1,
+    NRF_PWM2,
+    NRF_PWM3,
+#endif
+};
 static pwm_vars_t pwm_vars;
 
 //=========================== public ===========================================
 
-void db_pwm_init(const gpio_t *pins, size_t num_channels, uint16_t mtop) {
+void db_pwm_init(pwm_t pwm, const gpio_t *pins, size_t num_channels, uint16_t mtop) {
 
     // configure PWM channel pins;
     for (uint8_t channel = 0; channel < num_channels; channel++) {
         // Configure the PWM pins as output.
         db_gpio_init(&pins[channel], DB_GPIO_OUT);
-        DB_PWM->PSEL.OUT[channel] = (pins[channel].port << PWM_PSEL_OUT_PORT_Pos) |
-                                    (pins[channel].pin << PWM_PSEL_OUT_PIN_Pos) |
-                                    (PWM_PSEL_OUT_CONNECT_Connected << PWM_PSEL_OUT_CONNECT_Pos);
+        _pwm_devs[pwm]->PSEL.OUT[channel] = (pins[channel].port << PWM_PSEL_OUT_PORT_Pos) |
+                                            (pins[channel].pin << PWM_PSEL_OUT_PIN_Pos) |
+                                            (PWM_PSEL_OUT_CONNECT_Connected << PWM_PSEL_OUT_CONNECT_Pos);
     }
 
     // Enable the PWM peripheral
-    DB_PWM->ENABLE = (PWM_ENABLE_ENABLE_Enabled << PWM_ENABLE_ENABLE_Pos);
+    _pwm_devs[pwm]->ENABLE = (PWM_ENABLE_ENABLE_Enabled << PWM_ENABLE_ENABLE_Pos);
 
     // Configure PWM frequency and counting mode
-    DB_PWM->PRESCALER  = PWM_PRESCALER_PRESCALER_DIV_16;               // 1MHz clock
-    DB_PWM->COUNTERTOP = mtop;                                         // for example 100us period for the PWM signal means 10kHz
-    DB_PWM->MODE       = (PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos);  // UP counting mode
-    DB_PWM->LOOP       = (PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos);  // Disable single sequence looping feature
+    _pwm_devs[pwm]->PRESCALER  = PWM_PRESCALER_PRESCALER_DIV_16;               // 1MHz clock
+    _pwm_devs[pwm]->COUNTERTOP = mtop;                                         // for example 100us period for the PWM signal means 10kHz
+    _pwm_devs[pwm]->MODE       = (PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos);  // UP counting mode
+    _pwm_devs[pwm]->LOOP       = (PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos);  // Disable single sequence looping feature
 
     // Configure how many, and how the PWM dutycycles are loaded from memory
-    DB_PWM->DECODER = (PWM_DECODER_LOAD_Individual << PWM_DECODER_LOAD_Pos) |   // Have a different duty cycle value for each channel.
-                      (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);  // Reload the duty cycle values after every period, no delay
+    _pwm_devs[pwm]->DECODER = (PWM_DECODER_LOAD_Individual << PWM_DECODER_LOAD_Pos) |   // Have a different duty cycle value for each channel.
+                              (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);  // Reload the duty cycle values after every period, no delay
 
     // Configure the EasyDMA variables for loading the duty cycle values.
-    DB_PWM->SEQ[0].PTR = ((uint32_t)(pwm_vars.seq) << PWM_SEQ_PTR_PTR_Pos);
-    DB_PWM->SEQ[0].CNT = (DB_PWM_MAX_CHANNELS << PWM_SEQ_CNT_CNT_Pos);
+    _pwm_devs[pwm]->SEQ[0].PTR = ((uint32_t)(pwm_vars.seq) << PWM_SEQ_PTR_PTR_Pos);
+    _pwm_devs[pwm]->SEQ[0].CNT = (DB_PWM_MAX_CHANNELS << PWM_SEQ_CNT_CNT_Pos);
 
-    DB_PWM->SEQ[0].REFRESH  = 0UL;
-    DB_PWM->SEQ[0].ENDDELAY = 0UL;
+    _pwm_devs[pwm]->SEQ[0].REFRESH  = 0UL;
+    _pwm_devs[pwm]->SEQ[0].ENDDELAY = 0UL;
 
     // Activate the automatic looping of the PWM duty cycle sequence.
-    DB_PWM->SHORTS = (PWM_SHORTS_LOOPSDONE_SEQSTART0_Enabled << PWM_SHORTS_LOOPSDONE_SEQSTART0_Pos);
+    _pwm_devs[pwm]->SHORTS = (PWM_SHORTS_LOOPSDONE_SEQSTART0_Enabled << PWM_SHORTS_LOOPSDONE_SEQSTART0_Pos);
 
     // For safety, initialize all PWMs to zero.
     for (uint8_t channel = 0; channel < DB_PWM_MAX_CHANNELS; channel++) {
@@ -81,20 +90,20 @@ void db_pwm_init(const gpio_t *pins, size_t num_channels, uint16_t mtop) {
     }
 }
 
-void db_pwm_channel_set(uint8_t channel, uint16_t value) {
+void db_pwm_channel_set(pwm_t pwm, uint8_t channel, uint16_t value) {
     assert(channel < DB_PWM_MAX_CHANNELS);
 
     pwm_vars.seq[channel] = value | 1 << 15;
     // Update PWM values
-    DB_PWM->TASKS_SEQSTART[0] = PWM_TASKS_SEQSTART_TASKS_SEQSTART_Trigger;
+    _pwm_devs[pwm]->TASKS_SEQSTART[0] = PWM_TASKS_SEQSTART_TASKS_SEQSTART_Trigger;
 }
 
-void db_pwm_channels_set(uint16_t *values) {
+void db_pwm_channels_set(pwm_t pwm, uint16_t *values) {
     assert(sizeof(values) == DB_PWM_MAX_CHANNELS);
 
     for (uint8_t channel = 0; channel < DB_PWM_MAX_CHANNELS; channel++) {
         pwm_vars.seq[channel] = values[channel] | 1 << 15;
     }
     // Update PWM values
-    DB_PWM->TASKS_SEQSTART[0] = PWM_TASKS_SEQSTART_TASKS_SEQSTART_Trigger;
+    _pwm_devs[pwm]->TASKS_SEQSTART[0] = PWM_TASKS_SEQSTART_TASKS_SEQSTART_Trigger;
 }
--- bsp/pwm.h
+++ bsp/pwm.h
@@ -15,6 +15,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "gpio.h"
+#include "nrf.h"
+
+//=========================== defines ==========================================
+
+typedef uint8_t pwm_t;
 
 //=========================== public ===========================================
 
@@ -27,29 +32,32 @@
  * The number of channels must not exceed the size of the array containing
  * the pins.
  *
+ * @param[in] pwm           index of the PWM peripheral to initialize
  * @param[in] pins          pointer to array of pointer to GPIO pins
  * @param[in] num_channels  number of channels to configure
  * @param[in] mtop          max value of the PWM counter register
  */
-void db_pwm_init(const gpio_t *pins, size_t num_channels, uint16_t mtop);
+void db_pwm_init(pwm_t pwm, const gpio_t *pins, size_t num_channels, uint16_t mtop);
 
 /**
  * @brief Set the value of a PWM channel
  *
  * The channel value must be lower or equal to 3.
  *
+ * @param[in] pwm       index of the PWM peripheral
  * @param[in] channel   index of channel to set
  * @param[in] value     value to set to the channel
  */
-void db_pwm_channel_set(uint8_t channel, uint16_t value);
+void db_pwm_channel_set(pwm_t pwm, uint8_t channel, uint16_t value);
 
 /**
  * @brief Set the values of all PWM channels
  *
  * The array of values must be 4 bytes long exactly.
  *
+ * @param[in] pwm       index of the PWM peripheral
  * @param[in] values    pointer to the array containing the values to set
  */
-void db_pwm_channels_set(uint16_t *values);
+void db_pwm_channels_set(pwm_t pwm, uint16_t *values);
 
 #endif
