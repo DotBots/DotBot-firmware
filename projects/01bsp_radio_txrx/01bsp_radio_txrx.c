4677398aba61c92db7c83025854d131732fab73a
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-09-01 10:49:41 +0200

    projects/01bsp_radio_*: print RSSI of received packet

--- projects/01bsp_radio_txrx/01bsp_radio_txrx.c
+++ projects/01bsp_radio_txrx/01bsp_radio_txrx.c
@@ -41,7 +41,7 @@ static const gpio_t _dbg_pin = { .port = DB_LED1_PORT, .pin = DB_LED1_PIN };
 
 static void radio_callback(uint8_t *packet, uint8_t length) {
     db_gpio_toggle(&_dbg_pin);
-    printf("packet received (%dB): %s\n", length, (char *)packet);
+    printf("packet received (%dB): %s, RSSI: %i\n", length, (char *)packet, db_radio_rssi());
 }
 
 //=========================== main ==============================================
