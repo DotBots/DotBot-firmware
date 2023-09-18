b852ada06c5fcdfc0bf7ace4f4a0d308507fe264
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-09-18 14:52:20 +0200

    drv/rgbled_pwm: RGB LED driver is inverted

--- drv/rgbled_pwm/rgbled_pwm.c
+++ drv/rgbled_pwm/rgbled_pwm.c
@@ -25,9 +25,11 @@
 
 void db_rgbled_pwm_init(const db_rgbled_pwm_conf_t *conf) {
     db_pwm_init(1, conf->pins, PWM_CHANNELS, PWM_MAX_VALUE);
+    uint16_t pwm_seq[PWM_CHANNELS] = { UINT8_MAX, UINT8_MAX, UINT8_MAX };
+    db_pwm_channels_set(1, pwm_seq);
 }
 
 void db_rgbled_pwm_set_color(uint8_t red, uint8_t green, uint8_t blue) {
-    uint16_t pwm_seq[PWM_CHANNELS] = { red, green, blue };
+    uint16_t pwm_seq[PWM_CHANNELS] = { UINT8_MAX - red, UINT8_MAX - green, UINT8_MAX - blue };
     db_pwm_channels_set(1, pwm_seq);
 }
