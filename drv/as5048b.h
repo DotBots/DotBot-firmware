4057cec03d43daf409ab5e6a75e74302159e4b8d
Zimrahin <diegobadillosj@gmail.com>
2024-02-01 15:53:49 +0100

    drv: fix formatting issues using clang-format

--- drv/as5048b.h
+++ drv/as5048b.h
@@ -17,7 +17,6 @@
 #include <stdlib.h>
 #include <nrf.h>
 
-
 /**
  * @brief Initialise the AS5048B rotary encoder
  */
@@ -28,31 +27,30 @@ void as5048b_init(void);
  *
  * @param[out] angle_uint_out 14-bit raw angle (0x0 to 0x3FFF)
  */
-void as5048b_i2c_read_raw_angle(uint16_t* angle_uint_out);
+void as5048b_i2c_read_raw_angle(uint16_t *angle_uint_out);
 
 /**
  * @brief Reads angle as a float in radians
  *
  * @param[out] angle_rad_out Float angle [0, 2*M_PI)
  */
-void as5048b_i2c_read_angle_radian(float* angle_rad_out);
+void as5048b_i2c_read_angle_radian(float *angle_rad_out);
 
 /**
  * @brief Reads angle as a float in degrees
  *
  * @param[out] angle_deg_out Float angle [0, 360.)
  */
-void as5048b_i2c_read_angle_degree(float* angle_deg_out);
+void as5048b_i2c_read_angle_degree(float *angle_deg_out);
 
 /**
  * @brief Convert the raw angle to interval [0, max_angle)
  *
  * @param[in] raw_angle 14-bit raw angle (0x0 to 0x3FFF)
  * @param[in] max_angle Maximum angle for conversion
- * 
+ *
  * @return Float angle in interval [0, max_angle)
  */
 float as5048b_convert_raw_angle(uint16_t raw_angle, float max_angle);
-
 
 #endif
--- drv/as5048b/as5048b.c
+++ drv/as5048b/as5048b.c
@@ -16,21 +16,20 @@
 #include "i2c.h"
 #include "as5048b.h"
 
-
 //=========================== defines ==========================================
 // Define scl and sda gpio pins
-static const gpio_t scl = { .port = 0, .pin = 10 }; 
-static const gpio_t sda = { .port = 0, .pin = 9  };
+static const gpio_t scl = { .port = 0, .pin = 10 };
+static const gpio_t sda = { .port = 0, .pin = 9 };
 
 // Hardware settings of pins A1 and A2 define the last two bits of the slave address
 // Set A1 and A2 to GND to get default address 0x40
-#define AS5048B_SLAVE_ADDRESS   (0x40)
-#define SLAVE_ADDRESS_REG       (0x15)
+#define AS5048B_SLAVE_ADDRESS (0x40)
+#define SLAVE_ADDRESS_REG     (0x15)
 
 // Registers where absolute angle is stored
 // 14 bits in total, 6-13(MSB) and 0-5(LSB) respectively
-#define AS5048B_ANGLE_8MSB         (0xFE)
-#define AS5048B_ANGLE_6LSB         (0xFF)
+#define AS5048B_ANGLE_8MSB (0xFE)
+#define AS5048B_ANGLE_6LSB (0xFF)
 
 // Define Pi
 #ifndef M_PI
@@ -41,7 +40,7 @@ static const gpio_t sda = { .port = 0, .pin = 9  };
 
 // Initialise I2C communication with rotary encoder
 void as5048b_init(void) {
-    uint16_t    dummy_data;
+    uint16_t dummy_data;
 
     // AS5048B doesn't have a WHO_AM_I register
     db_i2c_init(&scl, &sda);
@@ -51,7 +50,7 @@ void as5048b_init(void) {
 }
 
 // Reads two 8-bit registers where the 14-bit raw absolute angle is stored
-void as5048b_i2c_read_raw_angle(uint16_t* angle_uint_out) {
+void as5048b_i2c_read_raw_angle(uint16_t *angle_uint_out) {
     uint8_t data_registers[2];
 
     // Read registers directly via I2C
@@ -65,26 +64,26 @@ void as5048b_i2c_read_raw_angle(uint16_t* angle_uint_out) {
 }
 
 // Reads angle as a float in radians
-void as5048b_i2c_read_angle_radian(float* angle_rad_out) {
-    uint16_t    angle_uint;
+void as5048b_i2c_read_angle_radian(float *angle_rad_out) {
+    uint16_t angle_uint;
     as5048b_i2c_read_raw_angle(&angle_uint);
 
     // Convert 14-bit raw angle (0x0 to 0x3FFF) to radians [0, 2*M_PI)
-    *angle_rad_out = (float) as5048b_convert_raw_angle(angle_uint, 2*M_PI);
+    *angle_rad_out = (float)as5048b_convert_raw_angle(angle_uint, 2 * M_PI);
 }
 
 // Reads angle as a float in degrees
-void as5048b_i2c_read_angle_degree(float* angle_deg_out) {
-    uint16_t    angle_uint;
+void as5048b_i2c_read_angle_degree(float *angle_deg_out) {
+    uint16_t angle_uint;
     as5048b_i2c_read_raw_angle(&angle_uint);
 
     // Convert 14-bit raw angle (0x0 to 0x3FFF) to degrees [0, 360)
-    *angle_deg_out = (float) as5048b_convert_raw_angle(angle_uint, 360.);
+    *angle_deg_out = (float)as5048b_convert_raw_angle(angle_uint, 360.);
 }
 
 // Convert the raw angle to [0, max_angle)
 float as5048b_convert_raw_angle(uint16_t raw_angle, float max_angle) {
     // Magic number is to exclude max_angle
-    float angle_out = ( (float)raw_angle / (float)0x3FFF ) * max_angle * (0.999938961118232313984);
+    float angle_out = ((float)raw_angle / (float)0x3FFF) * max_angle * (0.999938961118232313984);
     return angle_out;
 }
