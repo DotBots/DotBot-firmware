32913d0ad2e70da3a337d74161be1ecdf51197aa
Said Alvarado <said_alvarado@hotmail.com>
2024-02-15 16:02:46 +0100

    #268 LH2 (fix): wrong comment

--- bsp/lh2.h
+++ bsp/lh2.h
@@ -24,7 +24,7 @@
 //=========================== defines ==========================================
 
 #define LH2_BASESTATION_COUNT 4                          ///< Number of supported concurrent basestations
-#define LH2_POLYNOMIAL_COUNT  LH2_BASESTATION_COUNT * 2  ///< Number of supported concurrent basestations
+#define LH2_POLYNOMIAL_COUNT  LH2_BASESTATION_COUNT * 2  ///< Number of supported LFSR polynomials, two per basestation
 #define LH2_SWEEP_COUNT       2                          ///< Number of laser sweeps per basestations rotation
 
 /// LH2 data ready buffer state
