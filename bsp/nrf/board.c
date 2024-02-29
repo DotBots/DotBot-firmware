3929d1f5417226ac7ffe4d43ec2b589d0302b3ae
Said Alvarado <said_alvarado@hotmail.com>
2024-02-29 12:22:51 +0100

    minimote: start the minimote with the RGB LED shutdown

--- bsp/nrf/board.c
+++ bsp/nrf/board.c
@@ -28,9 +28,9 @@ static const gpio_t _reg_pin = { .port = DB_REGULATOR_PORT, .pin = DB_REGULATOR_
 
 void db_board_init(void) {
 
-// Make sure the mini-mote is running at 3.0v
-// Might need a re-start to take effect
 #if defined(BOARD_LH2_MINI_MOTE)
+    // Make sure the mini-mote is running at 3.0v
+    // Might need a re-start to take effect
     if (NRF_UICR->REGOUT0 != UICR_REGOUT0_VOUT_3V0) {
         NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
         while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
@@ -41,6 +41,21 @@ void db_board_init(void) {
         while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
         }
     }
+
+    // Start the mini-mote with the RGB LED turned off
+    static const gpio_t _r_led_pin = { .port = DB_RGB_LED_PWM_RED_PORT, .pin = DB_RGB_LED_PWM_RED_PIN };
+    static const gpio_t _g_led_pin = { .port = DB_RGB_LED_PWM_GREEN_PORT, .pin = DB_RGB_LED_PWM_GREEN_PIN };
+    static const gpio_t _b_led_pin = { .port = DB_RGB_LED_PWM_BLUE_PORT, .pin = DB_RGB_LED_PWM_BLUE_PIN };
+
+    // Configure RGB LED pins as output
+    db_gpio_init(&_r_led_pin, DB_GPIO_OUT);
+    db_gpio_init(&_g_led_pin, DB_GPIO_OUT);
+    db_gpio_init(&_b_led_pin, DB_GPIO_OUT);
+    // Shutdown the LED
+    db_gpio_set(&_reg_pin);
+    db_gpio_set(&_reg_pin);
+    db_gpio_set(&_reg_pin);
+
 #endif
 
 #if defined(DB_REGULATOR_PORT)
