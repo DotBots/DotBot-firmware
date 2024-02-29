f317598713a858dd5cd24ed2c155f2dccb6d8ee1
Said Alvarado <said_alvarado@hotmail.com>
2024-02-29 12:11:01 +0100

    minimote: Set vdd=3.0v in db_board_init()

--- bsp/nrf/board.c
+++ bsp/nrf/board.c
@@ -28,11 +28,24 @@ static const gpio_t _reg_pin = { .port = DB_REGULATOR_PORT, .pin = DB_REGULATOR_
 
 void db_board_init(void) {
 
+// Make sure the mini-mote is running at 3.0v
+// Might need a re-start to take effect
+#if defined(BOARD_LH2_MINI_MOTE)
+    if (NRF_UICR->REGOUT0 != UICR_REGOUT0_VOUT_3V0) {
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+        }
+        NRF_UICR->REGOUT0 = UICR_REGOUT0_VOUT_3V0;
+
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+        }
+    }
+#endif
+
 #if defined(DB_REGULATOR_PORT)
     // Turn ON the DotBot board regulator if provided
     db_gpio_init(&_reg_pin, DB_GPIO_OUT);
     db_gpio_set(&_reg_pin);
 #endif
 }
-
-// TODO: add a 3v minimote UICR code
