7f72ba708bf99231e90d26f2a516048f3d5e0bad
Raphaël Simoes <lepetitjin@hotmail.fr>
2024-02-29 15:22:55 +0100

    read commit description
    dist/scripts/radio_test/radio_test.py => updgrade python code use of hdlc
    radio_test/rx : add structure to manage packet from radio and HDLC
    encoding
    radio_test/tx : increase size of packet transmit, in progress gpio
    radio_test/tx_idle : in progress gpio IRQ
    bsp/radio:correct tx_start to be able to use tx

--- radio_test/rx/main_radio_test_rx.c
+++ radio_test/rx/main_radio_test_rx.c
@@ -17,10 +17,8 @@
 #include "gpio.h"
 #include "board_config.h"
 #include "radio.h"
+#include "hdlc.h"
 #include "timer.h"
-
-static const uint8_t write_crc[3] = "CRC";
-static const uint8_t uart_end     = 0x00;
 
 #define DB_BUFFER_MAX_BYTES (255U)       ///< Max bytes in UART receive buffer
 #define DB_UART_BAUDRATE    (1000000UL)  ///< UART baudrate used by the gateway
@@ -30,23 +28,33 @@ static const uint8_t uart_end     = 0x00;
 #define DB_UART_INDEX (0)  ///< Index of UART peripheral to use
 #endif
 
+typedef struct __attribute__((packed)) {
+    uint8_t payload[98];
+    int8_t  rssi;
+    bool    crc;
+} radio_test_data_t;
+
+static radio_test_data_t _data = { 0 };
+
+static uint8_t hdlc_tx_buffer[DB_BUFFER_MAX_BYTES];
+static size_t  hdlc_tx_buffer_size;
+
+int i;
+
 static void _led1_blink_fast(void) {
     db_gpio_toggle(&db_led1);
 }
 
 static void _radio_callback(uint8_t *packet, uint8_t length, bool crc) {
-    if (!crc) {
-        memcpy(packet + length, &write_crc, 3);
-        db_uart_write(DB_UART_INDEX, packet, length + 3);
-    }
-    uint8_t RSSI     = db_radio_rssi();
-    packet[length++] = RSSI;
-    packet[length++] = uart_end;
-    db_uart_write(DB_UART_INDEX, packet, length);
-}
 
-static void _uart_callback(uint8_t data) {
-    db_uart_write(DB_UART_INDEX, &data, 1);
+    for (i = 0; i < length; i++) {
+        _data.payload[i] = packet[i];
+    }
+    _data.rssi = db_radio_rssi();
+    _data.crc  = crc;
+
+    hdlc_tx_buffer_size = db_hdlc_encode((uint8_t *)&_data, sizeof(radio_test_data_t), hdlc_tx_buffer);
+    db_uart_write(DB_UART_INDEX, hdlc_tx_buffer, hdlc_tx_buffer_size);
 }
 
 int main(void) {
@@ -59,7 +67,7 @@ int main(void) {
     db_radio_set_frequency(8);  // Set the RX frequency to 2408 MHz.
     db_radio_rx();              // Start receiving packets.
 
-    db_uart_init(DB_UART_INDEX, &db_uart_rx, &db_uart_tx, DB_UART_BAUDRATE, &_uart_callback);
+    db_uart_init(DB_UART_INDEX, &db_uart_rx, &db_uart_tx, DB_UART_BAUDRATE, NULL);
 
     while (1) {}
 }
--- radio_test/tx_idle/main_radio_test_tx_idle.c
+++ radio_test/tx_idle/main_radio_test_tx_idle.c
@@ -21,13 +21,14 @@
 #if defined(NRF5340_XXAA) && defined(NRF_APPLICATION)
 #define RADIO_TXPOWER_TXPOWER_0dBm 0
 #endif
+static const gpio_t db_gpio_0_9 = { .port = 0, .pin = 9 };
 
 static void _led1_blink_slow(void) {
     db_gpio_toggle(&db_led1);
 }
-
-static void _radio_callback(uint8_t *packet, uint8_t length, bool crc) {
-    printf("packet received (%dB): %s, RSSI: %i, CRC: %d\n", length, (char *)packet, crc, db_radio_rssi());
+static void _gpio_callback(void *ctx) {
+    const gpio_t *gpio = (const gpio_t *)ctx;
+    db_gpio_clear(gpio);
 }
 
 int main(void) {
@@ -35,8 +36,9 @@ int main(void) {
     db_gpio_set(&db_led1);
     db_timer_init();
     db_timer_set_periodic_ms(0, 2000, _led1_blink_slow);
+    db_gpio_init_irq(&db_gpio_0_9, DB_GPIO_IN_PU, DB_GPIO_IRQ_EDGE_RISING, _gpio_callback, (void *)&db_led2);
 
-    db_radio_init(&_radio_callback, DB_RADIO_BLE_1MBit);
+    db_radio_init(NULL, DB_RADIO_BLE_1MBit);
     db_radio_set_frequency(8);  // Set the RX frequency to 2408 MHz.
     db_radio_set_tx_power(RADIO_TXPOWER_TXPOWER_0dBm);
     db_radio_tx_start();  // Start receiving packets.
