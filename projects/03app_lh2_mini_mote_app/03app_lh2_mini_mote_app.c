56e6e9b748c402eaf41fbafa8927d878c22cd23f
Said Alvarado <said_alvarado@hotmail.com>
2024-03-01 12:31:14 +0100

    minimote: minimote app done
    programmed to send LH2 packets to the gateway as fast as possible. Tested with two lighthouses

--- projects/03app_lh2_mini_mote_app/03app_lh2_mini_mote_app.c
+++ projects/03app_lh2_mini_mote_app/03app_lh2_mini_mote_app.c
@@ -31,42 +31,15 @@
 
 //=========================== defines ==========================================
 
-#define DB_LH2_UPDATE_DELAY_US    (100000U)   ///< 100ms delay between each LH2 data refresh
-#define DB_ADVERTIZEMENT_DELAY_US (500000U)   ///< 500ms delay between each advertizement packet sending
-#define DB_TIMEOUT_CHECK_DELAY_US (200000U)   ///< 200ms delay between each timeout delay check
-#define TIMEOUT_CHECK_DELAY_TICKS (17000)  ///< ~500 ms delay between packet received timeout checks
-#define DB_LH2_FULL_COMPUTATION   (false)  ///< Wether the full LH2 computation is perform on board
-#define DB_LH2_COUNTER_MASK       (0x07)   ///< Maximum number of lh2 iterations without value received
-#define DB_BUFFER_MAX_BYTES       (255U)   ///< Max bytes in UART receive buffer
-#define DB_DIRECTION_THRESHOLD    (0.01)   ///< Threshold to update the direction
-#define DB_DIRECTION_INVALID      (-1000)  ///< Invalid angle e.g out of [0, 360] range
-#define DB_MAX_SPEED              (60)     ///< Max speed in autonomous control mode
-#if defined(BOARD_DOTBOT_V2)
-#define DB_REDUCE_SPEED_FACTOR  (0.7)  ///< Reduction factor applied to speed when close to target or error angle is too large
-#define DB_REDUCE_SPEED_ANGLE   (25)   ///< Max angle amplitude where speed reduction factor is applied
-#define DB_ANGULAR_SPEED_FACTOR (35)   ///< Constant applied to the normalized angle to target error
-#define DB_ANGULAR_SIDE_FACTOR  (-1)   ///< Angular side factor
-#else                                  // BOARD_DOTBOT_V1
-#define DB_REDUCE_SPEED_FACTOR  (0.9)  ///< Reduction factor applied to speed when close to target or error angle is too large
-#define DB_REDUCE_SPEED_ANGLE   (20)   ///< Max angle amplitude where speed reduction factor is applied
-#define DB_ANGULAR_SPEED_FACTOR (30)   ///< Constant applied to the normalized angle to target error
-#define DB_ANGULAR_SIDE_FACTOR  (1)    ///< Angular side factor
-#endif
+#define DB_LH2_UPDATE_DELAY_US    (100000U)  ///< 100ms delay between each LH2 data refresh
+#define DB_ADVERTIZEMENT_DELAY_US (500000U)  ///< 500ms delay between each advertizement packet sending
+#define DB_BUFFER_MAX_BYTES       (255U)     ///< Max bytes in UART receive buffer
 
 typedef struct {
     uint32_t                 ts_last_packet_received;            ///< Last timestamp in microseconds a control packet was received
     db_lh2_t                 lh2;                                ///< LH2 device descriptor
     uint8_t                  radio_buffer[DB_BUFFER_MAX_BYTES];  ///< Internal buffer that contains the command to send (from buttons)
-    protocol_lh2_location_t  last_location;                      ///< Last computed LH2 location received
-    int16_t                  direction;                          ///< Current direction of the DotBot (angle in °)
-    protocol_control_mode_t  control_mode;                       ///< Remote control mode
-    protocol_lh2_waypoints_t waypoints;                          ///< List of waypoints
-    uint32_t                 waypoints_threshold;                ///< Distance to target waypoint threshold
-    uint8_t                  next_waypoint_idx;                  ///< Index of next waypoint to reach
-    bool                     update_control_loop;                ///< Whether the control loop need an update
     bool                     advertize;                          ///< Whether an advertize packet should be sent
-    bool                     update_lh2;                         ///< Whether LH2 data must be processed
-    uint8_t                  lh2_update_counter;                 ///< Counter used to track when lh2 data were received and to determine if an advertizement packet is needed
     uint64_t                 device_id;                          ///< Device ID of the DotBot
     db_log_dotbot_data_t     log_data;
 } dotbot_vars_t;
@@ -74,12 +47,11 @@ typedef struct {
 //=========================== variables ========================================
 
 static dotbot_vars_t _dotbot_vars;
-static const gpio_t _r_led_pin = { .port = DB_RGB_LED_PWM_RED_PORT, .pin = DB_RGB_LED_PWM_RED_PIN };
+static const gpio_t  _r_led_pin = { .port = DB_RGB_LED_PWM_RED_PORT, .pin = DB_RGB_LED_PWM_RED_PIN };
 
 //=========================== prototypes =======================================
 
 static void _advertise(void);
-static void _update_lh2(void);
 static void _turn_off_led(void);
 static void radio_callback(uint8_t *pkt, uint8_t len);
 
@@ -108,62 +80,58 @@ int main(void) {
     // Setup up timer interrupts
     db_timer_hf_init();
     db_timer_hf_set_periodic_us(1, DB_ADVERTIZEMENT_DELAY_US, &_advertise);
-    db_timer_hf_set_periodic_us(2, DB_LH2_UPDATE_DELAY_US, &_update_lh2);
     db_lh2_init(&_dotbot_vars.lh2, &db_lh2_d, &db_lh2_e);
     db_lh2_start();
 
     while (1) {
         __WFE();
 
-        bool need_advertize = false;
         // Process available lighthouse data
         db_lh2_process_location(&_dotbot_vars.lh2);
 
-        if (_dotbot_vars.update_lh2) {
-            // Check if data is ready to send
-            for (size_t basestation = 0; basestation < 2; basestation++) {
-                for (size_t sweep = 0; sweep < 2; sweep++) {
-                    if (_dotbot_vars.lh2.data_ready[sweep][basestation] == DB_LH2_PROCESSED_DATA_AVAILABLE) {
+        // Check if data is ready to send
+        for (size_t basestation = 0; basestation < 2; basestation++) {
+            for (size_t sweep = 0; sweep < 2; sweep++) {
+                if (_dotbot_vars.lh2.data_ready[sweep][basestation] == DB_LH2_PROCESSED_DATA_AVAILABLE) {
 
-                        db_gpio_clear(&_r_led_pin);
-                        db_timer_hf_set_oneshot_us(0 , 3000, &_turn_off_led);
+                    db_gpio_clear(&_r_led_pin);
+                    db_timer_hf_set_oneshot_us(0, 3000, &_turn_off_led);
 
-                        // Prepare the radio buffer
-                        db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, LH2_mini_mote, DB_PROTOCOL_LH2_PROCESSED_DATA);
+                    // Prepare the radio buffer
+                    db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, LH2_mini_mote, DB_PROTOCOL_LH2_PROCESSED_DATA);
 
-                        // Package data into a variable
-                        protocol_lh2_processed_packet_t lh2_packet;
-                        lh2_packet.selected_polynomial = _dotbot_vars.lh2.locations[sweep][basestation].selected_polynomial;
-                        lh2_packet.lfsr_location       = _dotbot_vars.lh2.locations[sweep][basestation].lfsr_location;
-                        lh2_packet.delay_us            = db_timer_hf_now() - _dotbot_vars.lh2.timestamps[sweep][basestation];
+                    // Package data into a variable
+                    protocol_lh2_processed_packet_t lh2_packet;
+                    lh2_packet.selected_polynomial = _dotbot_vars.lh2.locations[sweep][basestation].selected_polynomial;
+                    lh2_packet.lfsr_location       = _dotbot_vars.lh2.locations[sweep][basestation].lfsr_location;
+                    lh2_packet.delay_us            = db_timer_hf_now() - _dotbot_vars.lh2.timestamps[sweep][basestation];
 
-                        // Add the LH2 sweep
-                        memcpy(_dotbot_vars.radio_buffer + sizeof(protocol_header_t), &lh2_packet, sizeof(protocol_lh2_processed_packet_t));
-                        size_t length = sizeof(protocol_header_t) + sizeof(protocol_lh2_processed_packet_t);
+                    // Add the LH2 sweep
+                    memcpy(_dotbot_vars.radio_buffer + sizeof(protocol_header_t), &lh2_packet, sizeof(protocol_lh2_processed_packet_t));
+                    size_t length = sizeof(protocol_header_t) + sizeof(protocol_lh2_processed_packet_t);
 
+                    // Send through radio
+                    db_radio_disable();
+                    db_radio_tx(_dotbot_vars.radio_buffer, length);
 
-                        // Send through radio
-                        db_radio_disable();
-                        db_radio_tx(_dotbot_vars.radio_buffer, length);
+                    // Mark the data as already sent
+                    _dotbot_vars.lh2.data_ready[sweep][basestation] = DB_LH2_NO_NEW_DATA;
 
-                        // Mark the data as already sent
-                        _dotbot_vars.lh2.data_ready[sweep][basestation] = DB_LH2_NO_NEW_DATA;
-                        db_timer_hf_delay_us(200);
-                        // busy loop hf_delay_us hangs if I use it here
-                    }
+                    // busy loop - hf_delay_us hangs if I use it here
+                    uint32_t busy_loop = db_timer_hf_now() + 500;
+                    while (db_timer_hf_now() < busy_loop)
+                        ;
                 }
             }
-
-            _dotbot_vars.update_lh2 = false;
         }
+    }
 
-        if (_dotbot_vars.advertize && need_advertize) {
-            db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_ADVERTISEMENT);
-            size_t length = sizeof(protocol_header_t);
-            db_radio_disable();
-            db_radio_tx(_dotbot_vars.radio_buffer, length);
-            _dotbot_vars.advertize = false;
-        }
+    if (_dotbot_vars.advertize) {
+        db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_ADVERTISEMENT);
+        size_t length = sizeof(protocol_header_t);
+        db_radio_disable();
+        db_radio_tx(_dotbot_vars.radio_buffer, length);
+        _dotbot_vars.advertize = false;
     }
 }
 
@@ -171,10 +139,6 @@ int main(void) {
 
 static void _advertise(void) {
     _dotbot_vars.advertize = true;
-}
-
-static void _update_lh2(void) {
-    _dotbot_vars.update_lh2 = true;
 }
 
 static void _turn_off_led(void) {
