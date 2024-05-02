e73e8897ae1db7d01553234e30bd584dc4b177c2
Alexandre Abadie <alexandre.abadie@inria.fr>
2024-05-02 09:33:24 +0200

    projects/03app_dotbot_gateway*: fix radio mode configuration support

--- projects/03app_dotbot_gateway/03app_dotbot_gateway.c
+++ projects/03app_dotbot_gateway/03app_dotbot_gateway.c
@@ -1,17 +1,3 @@
-/**
- * @file
- * @defgroup project_dotbot_gateway    DotBot Gateway application
- * @ingroup projects
- * @brief This is the DotBot gateway application
- *
- * Flash this application on an nRF5xxDK board and connect it to your computer
- * via USB.
- *
- * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
- * @author Alexandre Abadie <alexandre.abadie@inria.fr>
- * @copyright Inria, 2022-2023
- */
-
 #include <nrf.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -26,8 +12,6 @@
 #include "radio.h"
 #include "timer.h"
 #include "uart.h"
-
-#include "conf.h"
 
 //=========================== defines ==========================================
 
---
+++ projects/03app_dotbot_gateway_lr/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @defgroup project_dotbot_gateway    DotBot Gateway application
+ * @ingroup projects
+ * @brief This is the DotBot gateway application
+ *
+ * Flash this application on an nRF5xxDK board and connect it to your computer
+ * via USB.
+ *
+ * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
+ * @author Alexandre Abadie <alexandre.abadie@inria.fr>
+ * @copyright Inria, 2022-2023
+ */
+
+#include "conf.h"
+
+#include "../03app_dotbot_gateway/03app_dotbot_gateway.c"
