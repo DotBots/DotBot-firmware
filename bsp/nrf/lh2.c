1414cd9465128ec893fcdf800ef7aaa564503c04
Alexandre Abadie <alexandre.abadie@inria.fr>
2024-05-03 12:29:59 +0200

    bsp/lh2: cleanup copyright

--- bsp/nrf/lh2.c
+++ bsp/nrf/lh2.c
@@ -4,7 +4,9 @@
  *
  * @brief  nRF52833-specific definition of the "lh2" bsp module.
  *
- * @author Filip Maksimovic <filip.maksimovic@inria.fr>, Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
+ * @author Filip Maksimovic <filip.maksimovic@inria.fr>
+ * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
+ * @author Alexandre Abadie <alexandre.abadie@inria.fr>
  *
  * @copyright Inria, 2022
  */
