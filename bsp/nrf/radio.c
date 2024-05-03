e7e16e0a1f2e4ed1a3e6da0c01ec6d9ffb0a5506
Said Alvarado <said_alvarado@hotmail.com>
2024-05-03 12:09:38 +0200

    bsp/radio: fix typo on comment
    Co-authored-by: Alexandre Abadie <alexandre.abadie@inria.fr>

--- bsp/nrf/radio.c
+++ bsp/nrf/radio.c
@@ -30,7 +30,7 @@
 #define RADIO_INTERRUPT_PRIORITY 1
 #endif
 
-#define RADIO_TIFS          0U  ///< Inter frame spacing in us. zero maens IFS is enforced by software, not the hardware
+#define RADIO_TIFS          0U  ///< Inter frame spacing in us. zero means IFS is enforced by software, not the hardware
 #define RADIO_SHORTS_COMMON (RADIO_SHORTS_READY_START_Enabled << RADIO_SHORTS_READY_START_Pos) |                 \
                                 (RADIO_SHORTS_END_DISABLE_Enabled << RADIO_SHORTS_END_DISABLE_Pos) |             \
                                 (RADIO_SHORTS_ADDRESS_RSSISTART_Enabled << RADIO_SHORTS_ADDRESS_RSSISTART_Pos) | \
