4a6633dbc2937e61b56dc7dae274e87029df4860
Alexandre Abadie <alexandre.abadie@inria.fr>
2024-05-13 14:48:52 +0200

    projects/01bsp_i2c: adapt to new I2C API

--- projects/01bsp_i2c/01bsp_i2c.c
+++ projects/01bsp_i2c/01bsp_i2c.c
@@ -29,6 +29,8 @@
 #define LSM303AGR_OUT_Z_L_A    (0x2C)
 #define LSM303AGR_OUT_Z_H_A    (0x2D)
 
+#define I2C_DEV (0)
+
 typedef struct {
     int16_t x;  ///< X axis
     int16_t y;  ///< Y axis
@@ -37,16 +39,13 @@ typedef struct {
 
 //=========================== main =============================================
 
-/**
- *  @brief The program starts executing here.
- */
 int main(void) {
     db_board_init();
     db_timer_hf_init(0);
-    db_i2c_init(&db_scl, &db_sda);
-    db_i2c_begin();
+    db_i2c_init(I2C_DEV, &db_scl, &db_sda);
+    db_i2c_begin(I2C_DEV);
     uint8_t who_am_i;
-    db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_WHO_AM_I_REG, &who_am_i, 1);
+    db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_WHO_AM_I_REG, &who_am_i, 1);
     if (who_am_i != LSM303AGR_WHO_AM_I_VAL) {
         printf(
             "Invalid WHO_AM_I value '%d' (expected: %d)\n",
@@ -56,32 +55,29 @@ int main(void) {
 
     // enable the accelerometer
     uint8_t reg = 0x57;
-    db_i2c_write_regs(LSM303AGR_ADDR, LSM303AGR_CTRL1_A_REG, &reg, 1);
-    db_i2c_end();
+    db_i2c_write_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_CTRL1_A_REG, &reg, 1);
+    db_i2c_end(I2C_DEV);
 
     lsm303agr_acc_data_t data;
     uint8_t              tmp = 0;
 
     // read accelerometer data in a loop
     while (1) {
-        db_i2c_begin();
-        db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_OUT_X_L_A, &tmp, 1);
+        db_i2c_begin(I2C_DEV);
+        db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_OUT_X_L_A, &tmp, 1);
         data.x = tmp;
-        db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_OUT_X_H_A, &tmp, 1);
+        db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_OUT_X_H_A, &tmp, 1);
         data.x |= tmp << 8;
-        db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_OUT_Y_L_A, &tmp, 1);
+        db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_OUT_Y_L_A, &tmp, 1);
         data.y = tmp;
-        db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_OUT_Y_H_A, &tmp, 1);
+        db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_OUT_Y_H_A, &tmp, 1);
         data.y |= tmp << 8;
-        db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_OUT_Z_L_A, &tmp, 1);
+        db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_OUT_Z_L_A, &tmp, 1);
         data.z = tmp;
-        db_i2c_read_regs(LSM303AGR_ADDR, LSM303AGR_OUT_Z_H_A, &tmp, 1);
+        db_i2c_read_regs(I2C_DEV, LSM303AGR_ADDR, LSM303AGR_OUT_Z_H_A, &tmp, 1);
         data.z |= tmp << 8;
-        db_i2c_end();
+        db_i2c_end(I2C_DEV);
         printf("x: %i, y: %i, z: %i\n", data.x, data.y, data.z);
         db_timer_hf_delay_ms(0, 200);
     }
-
-    // one last instruction, doesn't do anything, it's just to have a place to put a breakpoint.
-    __NOP();
 }
