39b48805a2038c11c7b564c851ef0a05825922c1
Said Alvarado <said_alvarado@hotmail.com>
2024-05-30 16:24:28 +0200

    minimote: Same as above

--- projects/03app_lh2_mini_mote_test/03app_lh2_mini_mote_test.c
+++ projects/03app_lh2_mini_mote_test/03app_lh2_mini_mote_test.c
@@ -102,6 +102,7 @@ int main(void) {
                     lh2_packet.selected_polynomial = _dotbot_vars.lh2.locations[sweep][basestation].selected_polynomial;
                     lh2_packet.lfsr_location       = _dotbot_vars.lh2.locations[sweep][basestation].lfsr_location;
                     lh2_packet.delay_us            = db_timer_hf_now(0) - _dotbot_vars.lh2.timestamps[sweep][basestation];
+                    lh2_packet.timestamp_us            = _dotbot_vars.lh2.timestamps[sweep][basestation];
 
                     // Add the LH2 sweep
                     memcpy(_dotbot_vars.radio_buffer + sizeof(protocol_header_t), &lh2_packet, sizeof(protocol_lh2_processed_packet_t));
