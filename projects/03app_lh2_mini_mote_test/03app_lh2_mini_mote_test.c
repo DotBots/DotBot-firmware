7f18659277d09f861b4a8e4c43217e49380268bd
Said Alexander Alvarado Marin <said_alvarado@hotmail.com>
2024-06-03 17:09:48 +0200

    projects/03app_lh2_minimote_test: implement test project

--- projects/03app_lh2_mini_mote_test/03app_lh2_mini_mote_test.c
+++ projects/03app_lh2_mini_mote_test/03app_lh2_mini_mote_test.c
@@ -7,7 +7,6 @@
  * Test every functionality of the minimote, to check it works.
  * IMU orientation changes the color of the RGB led
  * The LED flashed white when it receives an LH2 packet.
- * TODO: currently it's a copy of the LH2_mini_mote App
  *
  * @author Said Alvarado-Marin <said-alexander.alvarado-marin@inria.fr>
  * @author Alexandre Abadie <alexandre.abadie@inria.fr>
@@ -29,6 +28,8 @@
 #include "rgbled_pwm.h"
 #include "timer_hf.h"
 #include "timer.h"
+// Include DRV headers
+#include "ism330.h"
 
 //=========================== defines ==========================================
 
@@ -36,24 +37,37 @@
 #define DB_ADVERTIZEMENT_DELAY_US (500000U)  ///< 500ms delay between each advertizement packet sending
 #define DB_BUFFER_MAX_BYTES       (255U)     ///< Max bytes in UART receive buffer
 
+typedef enum {
+    BLACK,
+    RED,
+    GREEN,
+    BLUE,
+} led_color_t;
+
 typedef struct {
-    uint32_t ts_last_packet_received;            ///< Last timestamp in microseconds a control packet was received
-    db_lh2_t lh2;                                ///< LH2 device descriptor
-    uint8_t  radio_buffer[DB_BUFFER_MAX_BYTES];  ///< Internal buffer that contains the command to send (from buttons)
-    bool     advertize;                          ///< Whether an advertize packet should be sent
-    uint64_t device_id;                          ///< Device ID of the DotBot
+    uint32_t    ts_last_packet_received;            ///< Last timestamp in microseconds a control packet was received
+    db_lh2_t    lh2;                                ///< LH2 device descriptor
+    uint8_t     radio_buffer[DB_BUFFER_MAX_BYTES];  ///< Internal buffer that contains the command to send (from buttons)
+    bool        advertize;                          ///< Whether an advertize packet should be sent
+    uint64_t    device_id;                          ///< Device ID of the DotBot
+    led_color_t color;                              ///< current color of the RBG defined by the IMU
 } dotbot_vars_t;
+
 
 //=========================== variables ========================================
 
 static dotbot_vars_t _dotbot_vars;
 static const gpio_t  _r_led_pin = { .port = DB_RGB_LED_PWM_RED_PORT, .pin = DB_RGB_LED_PWM_RED_PIN };
+static const gpio_t  _g_led_pin = { .port = DB_RGB_LED_PWM_GREEN_PORT, .pin = DB_RGB_LED_PWM_GREEN_PIN };
+static const gpio_t  _b_led_pin = { .port = DB_RGB_LED_PWM_BLUE_PORT, .pin = DB_RGB_LED_PWM_BLUE_PIN };
 
 //=========================== prototypes =======================================
 
 static void _advertise(void);
-static void _turn_off_led(void);
+static void _previous_led_color(void);
 static void radio_callback(uint8_t *pkt, uint8_t len);
+static void _imu_to_rgb(void);
+static void _set_rgb_led(void);
 
 //=========================== main =============================================
 
@@ -62,6 +76,7 @@ static void radio_callback(uint8_t *pkt, uint8_t len);
  */
 int main(void) {
     db_board_init();
+    db_ism330_init(&db_sda, &db_scl);
     db_protocol_init();
     db_radio_init(&radio_callback, DB_RADIO_BLE_1MBit);
     db_radio_set_frequency(8);  // Set the RX frequency to 2408 MHz.
@@ -92,7 +107,9 @@ int main(void) {
                 if (_dotbot_vars.lh2.data_ready[sweep][basestation] == DB_LH2_PROCESSED_DATA_AVAILABLE) {
 
                     db_gpio_clear(&_r_led_pin);
-                    db_timer_hf_set_oneshot_us(0, 0, 3000, &_turn_off_led);
+                    db_gpio_clear(&_b_led_pin);
+                    db_gpio_clear(&_g_led_pin);
+                    db_timer_hf_set_oneshot_us(0, 0, 3000, &_previous_led_color);
 
                     // Prepare the radio buffer
                     db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, LH2_mini_mote, DB_PROTOCOL_LH2_PROCESSED_DATA);
@@ -113,22 +130,26 @@ int main(void) {
 
                     // Mark the data as already sent
                     _dotbot_vars.lh2.data_ready[sweep][basestation] = DB_LH2_NO_NEW_DATA;
-
-                    // busy loop - hf_delay_us hangs if I use it here
-                    uint32_t busy_loop = db_timer_hf_now(0) + 500;
-                    while (db_timer_hf_now(0) < busy_loop)
-                        ;
                 }
             }
         }
-    }
 
-    if (_dotbot_vars.advertize) {
-        db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_ADVERTISEMENT);
-        size_t length = sizeof(protocol_header_t);
-        db_radio_disable();
-        db_radio_tx(_dotbot_vars.radio_buffer, length);
-        _dotbot_vars.advertize = false;
+        // busy loop - hf_delay_us hangs if I use it here
+        uint32_t busy_loop = db_timer_hf_now(0) + 100;
+        while (db_timer_hf_now(0) < busy_loop)
+            ;
+
+        // Check the IMU and change the color of the RBG LED
+        _imu_to_rgb();
+        _set_rgb_led();
+
+        if (_dotbot_vars.advertize) {
+            db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_ADVERTISEMENT);
+            size_t length = sizeof(protocol_header_t);
+            db_radio_disable();
+            db_radio_tx(_dotbot_vars.radio_buffer, length);
+            _dotbot_vars.advertize = false;
+        }
     }
 }
 
@@ -138,8 +159,68 @@ static void _advertise(void) {
     _dotbot_vars.advertize = true;
 }
 
-static void _turn_off_led(void) {
-    db_gpio_set(&_r_led_pin);
+static void _previous_led_color(void) {
+    _set_rgb_led();
+}
+
+static void _imu_to_rgb(void) {
+    // read IMU
+    static ism330_acc_data_t acc_data;
+    db_ism330_accel_read(&acc_data);
+
+    // Decide the color of the LED
+    // Calculate the absolute value.
+    if (acc_data.x < 0)
+        acc_data.x = acc_data.x * -1;
+    if (acc_data.y < 0)
+        acc_data.y = acc_data.y * -1;
+    if (acc_data.z < 0)
+        acc_data.z = acc_data.z * -1;
+
+    // Ligth an LED according to the direction of gravity.
+    if ((acc_data.x > acc_data.y) && (acc_data.x > acc_data.z)) {
+        _dotbot_vars.color = RED;
+    } else if ((acc_data.y > acc_data.x) && (acc_data.y > acc_data.z)) {
+        _dotbot_vars.color = GREEN;
+    } else if ((acc_data.z > acc_data.x) && (acc_data.z > acc_data.y)) {
+        _dotbot_vars.color = BLUE;
+    } else {
+        _dotbot_vars.color = BLACK;
+    }
+}
+
+static void _set_rgb_led(void) {
+    switch (_dotbot_vars.color) {
+        case RED:
+            db_gpio_clear(&_r_led_pin);
+            db_gpio_set(&_g_led_pin);
+            db_gpio_set(&_b_led_pin);
+            break;
+
+        case GREEN:
+            db_gpio_set(&_r_led_pin);
+            db_gpio_clear(&_g_led_pin);
+            db_gpio_set(&_b_led_pin);
+            break;
+
+        case BLUE:
+            db_gpio_set(&_r_led_pin);
+            db_gpio_set(&_g_led_pin);
+            db_gpio_clear(&_b_led_pin);
+            break;
+
+        case BLACK:
+            db_gpio_set(&_r_led_pin);
+            db_gpio_set(&_g_led_pin);
+            db_gpio_set(&_b_led_pin);
+            break;
+
+        default:
+            db_gpio_set(&_r_led_pin);
+            db_gpio_set(&_g_led_pin);
+            db_gpio_set(&_b_led_pin);
+            break;
+    }
 }
 
 //=========================== callbacks ========================================
