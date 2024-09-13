5818c548efe11080a8c27ad8273550e139023b14
Said Alvarado <said_alvarado@hotmail.com>
2024-09-13 11:42:00 +0200

    TDMA: port mini-mote app to use tdma

--- projects/03app_lh2_mini_mote_app/03app_lh2_mini_mote_app.c
+++ projects/03app_lh2_mini_mote_app/03app_lh2_mini_mote_app.c
@@ -25,18 +25,20 @@
 #include "radio.h"
 #include "rgbled_pwm.h"
 #include "timer_hf.h"
+// Include DRV headers
+#include "tdma_client.h"
 
 //=========================== defines ==========================================
 
-#define DB_LH2_UPDATE_DELAY_US    (100000U)  ///< 100ms delay between each LH2 data refresh
-#define DB_ADVERTIZEMENT_DELAY_US (500000U)  ///< 500ms delay between each advertizement packet sending
-#define DB_BUFFER_MAX_BYTES       (255U)     ///< Max bytes in UART receive buffer
+#define TIMER_DEV              (0)
+#define DB_RADIO_FREQ          (28)
+#define DB_LH2_UPDATE_DELAY_US (100000U)  ///< 100ms delay between each LH2 data refresh
+#define DB_BUFFER_MAX_BYTES    (255U)     ///< Max bytes in UART receive buffer
 
 typedef struct {
     uint32_t ts_last_packet_received;            ///< Last timestamp in microseconds a control packet was received
     db_lh2_t lh2;                                ///< LH2 device descriptor
     uint8_t  radio_buffer[DB_BUFFER_MAX_BYTES];  ///< Internal buffer that contains the command to send (from buttons)
-    bool     advertize;                          ///< Whether an advertize packet should be sent
     uint64_t device_id;                          ///< Device ID of the DotBot
 } dotbot_vars_t;
 
@@ -47,7 +49,6 @@ static const gpio_t  _r_led_pin = { .port = DB_RGB_LED_PWM_RED_PORT, .pin = DB_R
 
 //=========================== prototypes =======================================
 
-static void _advertise(void);
 static void _turn_off_led(void);
 static void radio_callback(uint8_t *pkt, uint8_t len);
 
@@ -59,20 +60,13 @@ static void radio_callback(uint8_t *pkt, uint8_t len);
 int main(void) {
     db_board_init();
     db_protocol_init();
-    db_radio_init(&radio_callback, DB_RADIO_BLE_1MBit);
-    db_radio_set_frequency(8);  // Set the RX frequency to 2408 MHz.
-    db_radio_rx();              // Start receiving packets.
-
-    // Set an invalid heading since the value is unknown on startup.
-    // Control loop is stopped and advertize packets are sent
-    _dotbot_vars.advertize = false;
+    db_tdma_client_init(&radio_callback, DB_RADIO_BLE_1MBit, DB_RADIO_FREQ);
 
     // Retrieve the device id once at startup
     _dotbot_vars.device_id = db_device_id();
 
     // Setup up timer interrupts
-    db_timer_hf_init(0);
-    db_timer_hf_set_periodic_us(0, 1, DB_ADVERTIZEMENT_DELAY_US, &_advertise);
+    db_timer_hf_init(TIMER_DEV);
     db_lh2_init(&_dotbot_vars.lh2, &db_lh2_d, &db_lh2_e);
     db_lh2_start();
 
@@ -88,7 +82,7 @@ int main(void) {
                 if (_dotbot_vars.lh2.data_ready[sweep][basestation] == DB_LH2_PROCESSED_DATA_AVAILABLE) {
 
                     db_gpio_clear(&_r_led_pin);
-                    db_timer_hf_set_oneshot_us(0, 0, 3000, &_turn_off_led);
+                    db_timer_hf_set_oneshot_us(TIMER_DEV, 0, 3000, &_turn_off_led);
 
                     // Prepare the radio buffer
                     db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, LH2_mini_mote, DB_PROTOCOL_LH2_PROCESSED_DATA);
@@ -104,8 +98,7 @@ int main(void) {
                     size_t length = sizeof(protocol_header_t) + sizeof(protocol_lh2_processed_packet_t);
 
                     // Send through radio
-                    db_radio_disable();
-                    db_radio_tx(_dotbot_vars.radio_buffer, length);
+                    db_tdma_client_tx(_dotbot_vars.radio_buffer, length);
 
                     // Mark the data as already sent
                     _dotbot_vars.lh2.data_ready[sweep][basestation] = DB_LH2_NO_NEW_DATA;
@@ -113,21 +106,9 @@ int main(void) {
             }
         }
     }
-
-    if (_dotbot_vars.advertize) {
-        db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_ADVERTISEMENT);
-        size_t length = sizeof(protocol_header_t);
-        db_radio_disable();
-        db_radio_tx(_dotbot_vars.radio_buffer, length);
-        _dotbot_vars.advertize = false;
-    }
 }
 
 //=========================== private functions ================================
-
-static void _advertise(void) {
-    _dotbot_vars.advertize = true;
-}
 
 static void _turn_off_led(void) {
     db_gpio_set(&_r_led_pin);
@@ -138,7 +119,7 @@ static void _turn_off_led(void) {
 static void radio_callback(uint8_t *pkt, uint8_t len) {
     (void)len;
 
-    _dotbot_vars.ts_last_packet_received = db_timer_hf_now(0);
+    _dotbot_vars.ts_last_packet_received = db_timer_hf_now(TIMER_DEV);
     uint8_t           *ptk_ptr           = pkt;
     protocol_header_t *header            = (protocol_header_t *)ptk_ptr;
     // Check destination address matches
