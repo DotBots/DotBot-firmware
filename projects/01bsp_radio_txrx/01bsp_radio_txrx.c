fcc422f923896b470c97e85bfc5c38935c33f79f
Zimrahin <diegobadillosj@gmail.com>
2024-10-18 15:18:21 +0200

    ieee802154: changes to fix GitHub checks

--- projects/01bsp_radio_txrx/01bsp_radio_txrx.c
+++ projects/01bsp_radio_txrx/01bsp_radio_txrx.c
@@ -22,10 +22,8 @@
 
 //=========================== defines ===========================================
 
-#define DELAY_MS             (100)  // Wait 100ms between each send
-#define CHANNEL              (15)   // 2425 MHz for IEEE 802.15.4; 2436 MHz for BLE
-#define DOTBOT_GW_RADIO_MODE DB_RADIO_BLE_1MBit
-// #define DOTBOT_GW_RADIO_MODE DB_RADIO_IEEE802154_250Kbit
+#define DELAY_MS (100)  // Wait 100ms between each send
+#define CHANNEL  (15)   // 2425 MHz for IEEE 802.15.4; 2436 MHz for BLE
 
 //=========================== variables =========================================
 
---
+++ projects/01bsp_radio_txrx/conf.h
@@ -0,0 +1,5 @@
+#include <nrf.h>
+#include "radio.h"
+
+#define DOTBOT_GW_RADIO_MODE DB_RADIO_BLE_1MBit
+// #define DOTBOT_GW_RADIO_MODE DB_RADIO_IEEE802154_250Kbit
