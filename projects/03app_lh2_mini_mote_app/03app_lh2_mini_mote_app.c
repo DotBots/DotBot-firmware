42047074dc34d2bba213b2ce63fdb9639a1d8c91
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-01-23 14:46:19 +0100

    projects/03app_lh2_mini_mote*: adapt to new protocol packet format and TDMA API changes

--- projects/03app_lh2_mini_mote_app/03app_lh2_mini_mote_app.c
+++ projects/03app_lh2_mini_mote_app/03app_lh2_mini_mote_app.c
@@ -31,11 +31,10 @@
 //=========================== defines ==========================================
 
 #define TIMER_DEV                 (0)
-#define DB_RADIO_FREQ             (8)              //< Set the frequency to 2408 MHz
-#define RADIO_APP                 (LH2_mini_mote)  // LH2 mini mote Radio App
-#define DB_LH2_UPDATE_DELAY_US    (100000U)        ///< 100ms delay between each LH2 data refresh
-#define DB_ADVERTISEMENT_DELAY_US (500000U)        ///< 500ms delay between each advertizement packet sending
-#define DB_BUFFER_MAX_BYTES       (255U)           ///< Max bytes in UART receive buffer
+#define DB_RADIO_FREQ             (8)        //< Set the frequency to 2408 MHz
+#define DB_LH2_UPDATE_DELAY_US    (100000U)  ///< 100ms delay between each LH2 data refresh
+#define DB_ADVERTISEMENT_DELAY_US (500000U)  ///< 500ms delay between each advertizement packet sending
+#define DB_BUFFER_MAX_BYTES       (255U)     ///< Max bytes in UART receive buffer
 
 typedef struct {
     uint32_t ts_last_packet_received;            ///< Last timestamp in microseconds a control packet was received
@@ -63,9 +62,8 @@ static void radio_callback(uint8_t *pkt, uint8_t len);
  */
 int main(void) {
     db_board_init();
-    db_protocol_init();
     _dotbot_vars.advertise = false;
-    db_tdma_client_init(&radio_callback, DB_RADIO_BLE_1MBit, DB_RADIO_FREQ, RADIO_APP);
+    db_tdma_client_init(&radio_callback, DB_RADIO_BLE_1MBit, DB_RADIO_FREQ);
 
     // Retrieve the device id once at startup
     _dotbot_vars.device_id = db_device_id();
@@ -91,7 +89,7 @@ int main(void) {
                     db_timer_hf_set_oneshot_us(TIMER_DEV, 0, 3000, &_turn_off_led);
 
                     // Prepare the radio buffer
-                    db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, LH2_mini_mote, DB_PROTOCOL_LH2_PROCESSED_DATA);
+                    size_t length = db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS);
 
                     // Package data into a variable
                     protocol_lh2_processed_packet_t lh2_packet;
@@ -100,8 +98,9 @@ int main(void) {
                     lh2_packet.timestamp_us        = _dotbot_vars.lh2.timestamps[sweep][basestation];
 
                     // Add the LH2 sweep
-                    memcpy(_dotbot_vars.radio_buffer + sizeof(protocol_header_t), &lh2_packet, sizeof(protocol_lh2_processed_packet_t));
-                    size_t length = sizeof(protocol_header_t) + sizeof(protocol_lh2_processed_packet_t);
+                    _dotbot_vars.radio_buffer[length++] = DB_PROTOCOL_LH2_PROCESSED_DATA;
+                    memcpy(_dotbot_vars.radio_buffer + length, &lh2_packet, sizeof(protocol_lh2_processed_packet_t));
+                    length += sizeof(protocol_lh2_processed_packet_t);
 
                     // Send through radio
                     db_tdma_client_tx(_dotbot_vars.radio_buffer, length);
@@ -113,8 +112,7 @@ int main(void) {
         }
 
         if (_dotbot_vars.advertise) {
-            db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_ADVERTISEMENT);
-            size_t length = sizeof(protocol_header_t);
+            size_t length = db_protocol_advertizement_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, LH2_mini_mote);
             db_tdma_client_tx(_dotbot_vars.radio_buffer, length);
             _dotbot_vars.advertise = false;
         }
@@ -137,8 +135,8 @@ static void radio_callback(uint8_t *pkt, uint8_t len) {
     (void)len;
 
     _dotbot_vars.ts_last_packet_received = db_timer_hf_now(TIMER_DEV);
-    uint8_t           *ptk_ptr           = pkt;
-    protocol_header_t *header            = (protocol_header_t *)ptk_ptr;
+    uint8_t                 *ptk_ptr     = pkt;
+    const protocol_header_t *header      = (const protocol_header_t *)ptk_ptr;
     // Check destination address matches
     if (header->dst != DB_BROADCAST_ADDRESS && header->dst != _dotbot_vars.device_id) {
         return;
@@ -149,18 +147,13 @@ static void radio_callback(uint8_t *pkt, uint8_t len) {
         return;
     }
 
-    // Check application is compatible
-    if (header->application != DotBot) {
-        return;
-    }
-
     uint8_t *cmd_ptr = ptk_ptr + sizeof(protocol_header_t);
     // parse received packet and update the motors' speeds
-    switch (header->type) {
+    switch ((uint8_t)*cmd_ptr++) {
 
         case DB_PROTOCOL_CMD_RGB_LED:
         {
-            protocol_rgbled_command_t *command = (protocol_rgbled_command_t *)cmd_ptr;
+            const protocol_rgbled_command_t *command = (const protocol_rgbled_command_t *)cmd_ptr;
             db_rgbled_pwm_set_color(command->r, command->g, command->b);
         } break;
 
